54a04246a5c314a08eb428777397cfb5dff23683
Paula Hightower <eurolife@gmail.com>
2021-12-16 16:16:24 +0000

    Setup NextJS, linting, Cypress in frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ['./src/components/**/*.{ts,tsx,js,jsx}', './src/pages/**/*.{ts,tsx,js,jsx}'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      red: colors.rose
+    },
+    extend: {},
+  },
+  plugins: [],
+}
