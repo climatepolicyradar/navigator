ce6b32b25eade391c373df95ef120f10187ae74f
Kalyan Dutia <kalyan.dutia@gmail.com>
2022-01-07 16:10:59 +0000

    Testing lookups APIs check the response length is the same as the number of rows in each DB (#136)

--- backend/app/api/api_v1/routers/tests/test_lookups.py
+++ backend/app/api/api_v1/routers/tests/test_lookups.py
@@ -1,17 +1,22 @@
-def test_get_lookups(client, user_token_headers):
+from app.db import models
+
+
+def test_get_lookups(client, user_token_headers, test_db):
 
     lookup_api_paths = [
-        "geographies",
-        "languages",
-        "action_types",
-        "sources",
+        ("geographies", models.Geography),
+        ("languages", models.Language),
+        ("action_types", models.ActionType),
+        ("sources", models.Source),
     ]
 
-    for path in lookup_api_paths:
+    for path, table in lookup_api_paths:
         response = client.get(
             f"/api/v1/{path}",
             headers=user_token_headers,
         )
 
+        db_data = test_db.query(table).all()
+
         assert response.status_code == 200
-        # assert len(response.json()) > 0
+        assert len(response.json()) == len(db_data)
