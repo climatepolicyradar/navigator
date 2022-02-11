967a1b93064b8983d8045350371c698c39df0bae
Paula Hightower <eurolife@gmail.com>
2022-02-11 17:53:05 +0000

    search input styling

--- frontend/src/components/buttons/SearchButton.tsx
+++ frontend/src/components/buttons/SearchButton.tsx
@@ -1,6 +1,6 @@
 const SearchButton = ({ children }) => {
   return (
-    <button className="bg-indigo-600 text-white uppercase p-2 rounded-full hover:bg-blue-500 transtion duration-300 w-24 font-medium">
+    <button className="bg-indigo-600 text-white uppercase p-2 rounded-full hover:bg-blue-500 transtion duration-300 w-24 shrink-0 font-medium">
       {children}
     </button>
   );
