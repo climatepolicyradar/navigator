5efd9fb01bcfd66d73fdffac350d181abac09d49
Paula Hightower <eurolife@gmail.com>
2022-02-21 17:30:15 +0000

    add test to ensure clear search button is not visible after clearing

--- frontend/e2e/cypress/integration/search-start.spec.js
+++ frontend/e2e/cypress/integration/search-start.spec.js
@@ -19,7 +19,7 @@ describe('Search Input', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000');
   });
-  it('Search input placeholder text should be visible', () => {
+  it('Search input placeholder text should be set', () => {
     cy.get('[data-cy="search-input"]').should(
       'have.attr',
       'placeholder',
@@ -27,11 +27,11 @@ describe('Search Input', () => {
     );
   });
   it('Clear search button should not be visible until typing a search term', () => {
-    cy.get('[data-cy="search-close-button"]').should('not.exist');
+    cy.get('[data-cy="search-clear-button"]').should('not.exist');
     cy.get('[data-cy="search-input"]').type('carbon taxes');
     cy.get('[data-cy="search-clear-button"]').should('exist');
   });
-  it('Clicking the clear search button should clear the search terms in the input box', () => {
+  it('Clicking the clear search button should clear the search terms in the input box and hide the button', () => {
     cy.get('[data-cy="search-input"]').type('carbon taxes');
     cy.get('[data-cy="search-input"]').should(
       'have.attr',
@@ -40,5 +40,6 @@ describe('Search Input', () => {
     );
     cy.get('[data-cy="search-clear-button"]').click();
     cy.get('[data-cy="search-input"]').should('have.attr', 'value', '');
+    cy.get('[data-cy="search-clear-button"]').should('not.exist');
   });
 });
