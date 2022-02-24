b6d8eec01f665971913d294a1721225189c11488
chrisaballard <chris@climatepolicyradar.org>
2022-02-24 14:12:32 +0000

    Added initial ability to the pipeline to process documents on s3 in a named folder.

--- pipeline/pdf2text.py
+++ pipeline/pdf2text.py
@@ -5,12 +5,54 @@ Implements a cli which will extract the text contained in a set of pdf files in 
 
 import argparse
 from pathlib import Path
+import tempfile
 
+from navigator.core.aws import S3Client, S3Document
 from extract.extract import DocumentEmbeddedTextExtractor
 from extract.exceptions import DocumentTextExtractorException
 
 
-def process(pdf_dir: Path, out_dir: Path, save_json: bool, save_text: bool):
+def split_s3_path(s3_bucket_path: str):
+    """"""
+    s3_bucket_path = Path(s3_bucket_path)
+    bucket = s3_bucket_path.parts[0]
+    folders = "/".join([folder for folder in s3_bucket_path.parts[1:]])
+
+    return bucket, folders
+
+
+def get_s3_file_path(s3_document: S3Document):
+    s3_document_path = Path(s3_document.key)
+    folders = "/".join([folder for folder in s3_document_path.parts[:-1]])
+
+    return s3_document_path, folders
+
+
+def get_files(pdf_path: str, use_s3: bool = False):
+    """Retrieve files from an s3 bucket and folder, or local directory
+
+    s3: [bucket]/[folder]/[folder]
+    """
+    if use_s3:
+        s3_client = S3Client()
+        bucket, folders = split_s3_path(pdf_path)
+        for s3_document in s3_client.list_files(bucket):
+            s3_document_path, s3_document_folders = get_s3_file_path(s3_document)
+            if s3_document_folders == folders and s3_document_path.suffix == ".pdf":
+                object_response = s3_client.download_file(s3_document)
+
+                temp_f = tempfile.NamedTemporaryFile(prefix="navigator_", suffix=".pdf")
+                temp_f.write(object_response.read())
+
+                yield Path(temp_f.name)
+    else:
+        for pdf_file in Path(pdf_path).glob("*.pdf"):
+            yield pdf_file
+
+
+def process(
+    pdf_path: str, out_dir: Path, save_json: bool, save_text: bool, use_s3: bool = False
+):
     """Extracts text from text in a directory containing pdf files.
 
     Iterates through files with a .pdf extension in a given directory,
@@ -26,7 +68,9 @@ def process(pdf_dir: Path, out_dir: Path, save_json: bool, save_text: bool):
 
     extractor = DocumentEmbeddedTextExtractor()
 
-    for pdf_file in pdf_dir.glob("*.pdf"):
+    for pdf_file in get_files(pdf_path, use_s3):
+        print(f"Processing {pdf_file}...")
+
         # Extract embedded text in pdf file
         pdf_doc = extractor.extract(pdf_file)
 
@@ -47,14 +91,14 @@ def configure_args():
         description="Extracts text from a directory containing pdf documents.",
     )
     parser.add_argument(
-        "pdf_dir",
+        "pdf_path",
         type=str,
-        help="Path to directory containing pdf files to process",
+        help="Path to directory or name of s3 bucket and subfolders containing pdf files to process",
     )
     parser.add_argument(
         "out_dir",
         type=str,
-        help="Path to directory to store output files",
+        help="Path to directory or name of s3 bucket and subfolders to store output files",
     )
     parser.add_argument(
         "--json",
@@ -67,6 +111,12 @@ def configure_args():
         action="store_true",
         default=False,
         help="Generates a text output file for each pdf document",
+    )
+    parser.add_argument(
+        "--s3",
+        action="store_true",
+        default=False,
+        help="Retrieve and write files to s3 buckets",
     )
 
     args = parser.parse_args()
@@ -83,17 +133,17 @@ def cli():
     # Configure and parse command line arguments
     args = configure_args()
 
-    pdf_dir = Path(args.pdf_dir)
+    pdf_path = Path(args.pdf_path)
     out_dir = Path(args.out_dir)
 
     # Check that the input/output directories exist and raise error if not
-    if not pdf_dir.exists():
+    if not pdf_path.exists() and not args.s3:
         raise DocumentTextExtractorException("Path to input pdf docuents is invalid")
     if not out_dir.exists():
         raise DocumentTextExtractorException("Output path is invalid")
 
     # Process the files in the directory
-    process(pdf_dir, out_dir, args.json, args.text),
+    process(pdf_path, out_dir, args.json, args.text, args.s3),
 
 
 if __name__ == "__main__":
