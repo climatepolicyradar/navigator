8192a7918a34743b3ad7495b2c1f1f8fa0c9b63f
Juan Uys <opyate@gmail.com>
2022-03-08 16:22:30 +0000

    revert sqlalchemy expire_on_commit

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,15 +1,14 @@
-from app.core import config
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+from app.core import config
 
 engine = create_engine(
     config.SQLALCHEMY_DATABASE_URI,
     pool_pre_ping=True,
 )
-SessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine, expire_on_commit=False
-)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 def make_declarative_base():
