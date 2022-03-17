c324eb4c74f3fc59386a8c244dad09a5a38d54ce
Juan Uys <opyate@gmail.com>
2022-03-17 09:39:40 +0000

    strip HTML from action description

--- loader/app/transform/test_main.py
+++ loader/app/transform/test_main.py
@@ -8,14 +8,16 @@ from app.transform import transform
 
 
 def test_transform():
-    policies_fe = pd.DataFrame({
-        'policy_name': ['foo', 'will be dropped as has no doc'],
-        'country_code': ['cc', 'xx'],
-        'document_list': ['doc name|http://doc|;second doc|http://doc2|en', None],
-        'policy_description': ['foobar', 'nodoc'],
-        'events': ['17/11/1979|Law passed', None],
-        'policy_type': ['legislative', 'legislative'],
-    })
+    policies_fe = pd.DataFrame(
+        {
+            "policy_name": ["foo", "will be dropped as has no doc"],
+            "country_code": ["cc", "xx"],
+            "document_list": ["doc name|http://doc|;second doc|http://doc2|en", None],
+            "policy_description": ["<div>foobar</div>", "nodoc"],
+            "events": ["17/11/1979|Law passed", None],
+            "policy_type": ["legislative", "legislative"],
+        }
+    )
 
     results: PolicyLookup = transform(policies_fe)
 
@@ -25,35 +27,37 @@ def test_transform():
     key: Key = keys[0]
     policy: PolicyData = results[key]
 
-    assert policy.policy_name == 'foo'
-    assert policy.policy_type == 'legislative'
-    assert policy.country_code == 'cc'
+    assert policy.policy_name == "foo"
+    assert policy.policy_type == "Law"
+    assert policy.country_code == "cc"
     assert policy.policy_date.date() == dt.date(1979, 11, 17)
-    assert policy.policy_description == 'foobar'
+    assert policy.policy_description == "foobar"
 
     docs: List[Doc] = policy.docs
     assert len(docs) == 2
 
     doc_one: Doc = docs[0]
-    assert doc_one.doc_name == 'doc name'
+    assert doc_one.doc_name == "doc name"
     assert doc_one.doc_language is None
-    assert doc_one.doc_url == 'http://doc'
+    assert doc_one.doc_url == "https://doc"
 
     doc_two: Doc = docs[1]
-    assert doc_two.doc_name == 'second doc'
-    assert doc_two.doc_language == 'en'
-    assert doc_two.doc_url == 'http://doc2'
+    assert doc_two.doc_name == "second doc"
+    assert doc_two.doc_language == "en"
+    assert doc_two.doc_url == "https://doc2"
 
 
 def test_transform_no_doc():
-    policies_fe = pd.DataFrame({
-        'policy_name': ['will be dropped as has no doc'],
-        'country_code': ['cc'],
-        'document_list': [None],
-        'policy_description': ['foobar'],
-        'events': ['17/11/1979|Law passed'],
-        'policy_type': ['legislative'],
-    })
+    policies_fe = pd.DataFrame(
+        {
+            "policy_name": ["will be dropped as has no doc"],
+            "country_code": ["cc"],
+            "document_list": [None],
+            "policy_description": ["foobar"],
+            "events": ["17/11/1979|Law passed"],
+            "policy_type": ["legislative"],
+        }
+    )
 
     results: PolicyLookup = transform(policies_fe)
 
@@ -62,14 +66,16 @@ def test_transform_no_doc():
 
 
 def test_transform_no_date():
-    policies_fe = pd.DataFrame({
-        'policy_name': ['will be dropped as has no date'],
-        'country_code': ['cc'],
-        'document_list': ['doc name|http://doc|;second doc|http://doc2|en'],
-        'policy_description': ['foobar'],
-        'events': [None],
-        'policy_type': ['legislative'],
-    })
+    policies_fe = pd.DataFrame(
+        {
+            "policy_name": ["will be dropped as has no date"],
+            "country_code": ["cc"],
+            "document_list": ["doc name|http://doc|;second doc|http://doc2|en"],
+            "policy_description": ["foobar"],
+            "events": [None],
+            "policy_type": ["legislative"],
+        }
+    )
 
     results: PolicyLookup = transform(policies_fe)
 
