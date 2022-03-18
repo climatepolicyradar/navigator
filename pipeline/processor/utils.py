d23fb321b7261dff275f67c680be592a91cc3646
Stefan <stefl141193@gmail.com>
2022-03-18 11:38:00 +0000

    Add util functiont to join hyphenated word fragments that should be a unique token.

--- pipeline/processor/utils.py
+++ pipeline/processor/utils.py
@@ -1,4 +1,6 @@
+import re
 from typing import List, Iterable
+from english_words import english_words_set
 
 
 def minimal_bounding_box(coords: List[Iterable]) -> list:
@@ -17,3 +19,32 @@ def minimal_bounding_box(coords: List[Iterable]) -> list:
     x_max = max(coord[2] for coord in coords)
     y_max = max(coord[3] for coord in coords)
     return [x_min, y_min, x_max, y_max]
+
+def rewrap_hyphenated_words(li: list) -> list:
+    """
+    Reorganise a list of strings so that word fragments separated
+    by hyphens or em dashes to start new lines are joined into a single
+    word if the word fragments have no meaning by themselves.
+
+    Args:
+        li: List of strings/sentences.
+
+    Returns:
+        List of strings/sentences with hyphenated words joined into a single word.
+
+    """
+    current = None
+    for ix, l in enumerate(li):
+        regex_match = re.search(r'\w+(-|–){1}$', l)
+        if current:
+            word_fragment = current.rstrip('-')
+            if word_fragment in english_words_set:
+                li[ix] = word_fragment + l
+            else:
+                li[ix] = word_fragment + l
+            current = None
+        if regex_match:
+            # Strip matching regex from the end of the string.
+            li[ix] = l[:regex_match.start()]
+            current = regex_match[0]
+    return li
