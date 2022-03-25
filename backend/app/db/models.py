01d3ace7cc77f9e21c58aa857d99d25407f9339b
Juan Uys <opyate@gmail.com>
2022-03-25 17:19:49 +0000

    new user fields

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -13,11 +13,17 @@ class User(Base):  # noqa: D101
 
     id = sa.Column(sa.Integer, primary_key=True)
     email = sa.Column(sa.String, unique=True, index=True, nullable=False)
-    first_name = sa.Column(sa.String)
-    last_name = sa.Column(sa.String)
+    names = sa.Column(sa.String)
     hashed_password = sa.Column(sa.String, nullable=False)
     is_active = sa.Column(sa.Boolean, default=True, nullable=False)
     is_superuser = sa.Column(sa.Boolean, default=False, nullable=False)
+    job_role = sa.Column(sa.String, nullable=True)
+    location = sa.Column(sa.String, nullable=True)
+    affiliation_organisation = sa.Column(sa.String, nullable=True)
+    affiliation_type = sa.Column(sa.ARRAY(sa.Text), nullable=True)
+    policy_type_of_interest = sa.Column(sa.ARRAY(sa.Text), nullable=True)
+    geographies_of_interest = sa.Column(sa.ARRAY(sa.Text), nullable=True)
+    data_focus_of_interest = sa.Column(sa.ARRAY(sa.Text), nullable=True)
 
 
 class PassageType(Base):  # noqa: D101
