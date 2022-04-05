764fbfdf080f3a5230381be723d7f93e9638a085
stefl14 <stefl141193@gmail.com>
2022-04-05 13:52:29 +0100

    Relatively large commit here as I've been experimenting with a bunch of stuff while forgetting to commit. In a nutshell:
    1) I tried some nuanced stuff with list processing, but in the end a simple approach worked best
    on the examples I tried (not too many edge cases). Any block subsequent to a list, irrespective
    of its page, is considered context or part of the same list on the previous page. There isn't too
    many failure modes so far as I can tell.
    
    2) Get text from merged block is where most of the interesting stuff happens. One thing I noticed was
    that the current postprocessing pipeline does not handle some styling cases. In particular, italicisation can produce missing semantics within a block. This is rare, but needs to be fixed.
    See comments r.e. exception handling. in get_text_from_merged_block function.
    
    3) I also noticed that we probably shouldn't be removing superscripts in the case where we have 1st,
    2nd, etc as these are likely capturable semantics. Dealt with using a simple regex.

--- search-index/text2embeddings.py
+++ search-index/text2embeddings.py
@@ -9,6 +9,7 @@ from tqdm.auto import tqdm
 import numpy as np
 import pandas as pd
 import click
+import re
 
 from app.ml import SBERTEncoder, SentenceEncoder
 from app.utils import paginate_list
@@ -18,10 +19,108 @@ from navigator.core.utils import get_timestamp
 logger = get_logger(__name__)
 
 
+def get_text_from_list(text_block: dict, prev_processed_text_block: dict) -> str:
+    """
+    Format a list text block and prepend it to the previous text block, assuming it is the context of the list.
+    This is a fairly strong assumption, but works most of the time. Code to handle more complex cases has been started.
+
+    Args:
+        text_block (dict): text block dict.
+        prev_processed_text_block (dict): previous text block dict.
+
+    Returns:
+        str: Formatted list text block that is useful for indexing semantics.
+    """
+    text = text_block["custom_attributes"]["pretty_list_string"]
+    text = text.strip()
+    text = text.replace("*", "")  # Remove bullet points.
+    # Always append previous text block as context. This is a fairly strong assumption, and the code to make this
+    # better has been started, in postprocessor.py, but is not yet completely robust, so we are not using it for now.
+    # TODO: Note, we can potentially do this in a more robust way by using the text block custom attributes created
+    #  in postprocessor.py, which I've updated. This will allow us to deal with edge cases, for example where the
+    #  previous block is not either context or part of the same list from the previous page. This would be the case
+    #  when, for example, there are random elements between pages that the postprocessors haven't been able to catch.
+    #  But I've ignored this for now because it seems that the postprocessing in postprocessor.py has dealt with the
+    #  vast majority of edge cases (as indicated by the rarity of certain debugging metadata).
+    text = prev_processed_text_block + "\n" + text
+    return text
+
+
+def delete_string_indices(data: str, indices: List[int]) -> str:
+    """Delete a list of indices from a string.
+    Args:
+        data (str): string to delete indices from.
+        indices (List[int]): list of indices to delete.
+    Returns:
+        str: string with indices deleted.
+    """
+    return "".join([char for idx, char in enumerate(data) if idx not in indices])
+
+
+def get_text_from_merged_block(text_block: dict) -> str:
+    """
+    Remove unnecessary styling from merged text blocks (superscripts).
+
+    Args:
+        text_block: A block of text that has been merged.
+
+    Returns:
+        str: The text block with styling that is not related to semantics removed.
+
+    """
+    # Text output with no processing.
+    text_output = (
+        "\n".join(text_block["text"]).strip(),
+        text_block["text_block_id"],
+    )
+    # Remove superscripts only. Bolding has no impact on the block's content once merged, so we don't need to remove
+    # anything. Subscripts are kept because, for example, we want to keep CO2 even when it is subscripted).
+    # Superscripts are almost always references not related to semantics, so we can remove them.
+    try:
+        style_spans = text_block["custom_attributes"]["styleSpans"]
+        delete_indices = []
+        for style_span in style_spans:
+            if style_span["style"] == "superscript":
+                remove_start = style_span["start_idx"]
+                remove_end = style_span["end_idx"]
+                # if superscript is of the form st, nd, rd, th, do not remove as this can potentially
+                # be used by language models.
+                delete_substr = text_output[0][remove_start:remove_end + 1]
+                if re.match(r"(st|nd|rd|th)$", delete_substr):
+                    continue # don't remove
+                delete_indices.extend([remove_start, remove_end])
+
+        # The indices are relative to the whole block, not just elements in the block.
+        # Remove the indices from deleted_indices from text_output.
+        if delete_indices:  # There will only be indices to delete in superscript case.
+            text_output_amended = delete_string_indices(text_output[0], delete_indices)
+            text_output = (text_output_amended, text_block["text_block_id"])
+            return text_output
+        else:
+            return text_output
+    # Blocks are sometimes merged by the styling processor for rare reasons other
+    # than style, in which case there is no stylespan attribute, hence this exception handling.
+    # This happens when, for example, there is a block with a unique path between two blocks with the same path.
+    # For example, italicized text often has a different path to the block it is part of because it
+    # is considered a span element. The styling processor as is does not handle this case, as for some
+    # reason Adobe doesn't consider italicization a style, and so the code is not robust to this. This
+    # is something that is probably easy to fix, but it's relatively rare so is left for now and is caught
+    # by the exception.
+    except KeyError:
+        # TODO: Fix problem that creates need for this exception (see comment above) handling (low priority).
+        #  c.f page 17 of cclw-9460 first block for an example.
+        logger.debug(
+            f"No style spans found for this merged text block. Some semantics may be missing from this block (e.g. "
+            f"italics). "
+        )
+        return text_output
+
+
 def get_text_from_document_dict(document: dict) -> List[Tuple[str, str]]:
     """Get the text and ID from each text block in a .json file created from a `Document` object.
 
-    A string is created for a text block by newline-joining its list of text.
+    A string is created for a text block by removing unnecessary styling elements, and removing
+    semantically irrelevant content from list types.
 
     Args:
         document (dict): dict created from a `Document` object, e.g. imported from a JSON file.
@@ -31,12 +130,18 @@ def get_text_from_document_dict(document: dict) -> List[Tuple[str, str]]:
     """
 
     text_output = []
-
+    prev_block_processed_text = None
     for page in document["pages"]:
         for text_block in page["text_blocks"]:
-            text_output.append(
-                ("\n".join(text_block["text"]).strip(), text_block["text_block_id"])
-            )
+            if text_block["type"] == "merged_text_block":
+                text = get_text_from_merged_block(text_block)
+            elif text_block["type"] == "list":
+                text = get_text_from_list(text_block, prev_block_processed_text)
+            else:
+                text = "\n".join(text_block["text"]).strip()
+            text_output.append((text, text_block["text_block_id"]))
+            # Keep previous text block in case we need to prepend it to a list as context.
+            prev_block_processed_text = text
 
     return text_output
 
@@ -65,7 +170,7 @@ def get_text_from_json_files(filepaths: List[str]) -> List[Tuple[str, str, str]]
 
 
 def encode_text(
-    text_list: List[str], encoder: SentenceEncoder, batch_size: int
+        text_list: List[str], encoder: SentenceEncoder, batch_size: int
 ) -> np.ndarray:
     """Encode list of text strings using a SentenceEncoder, in batches of `batch_size`
 
@@ -114,11 +219,11 @@ def encode_text(
     help="Optionally limit the number of text samples to process. Useful for debugging.",
 )
 def run_cli(
-    input_dir: Path,
-    output_dir: Path,
-    model_name: str,
-    batch_size: int,
-    limit: Optional[int],
+        input_dir: Path,
+        output_dir: Path,
+        model_name: str,
+        batch_size: int,
+        limit: Optional[int],
 ):
     """Run CLI to produce embeddings from pdf2text JSON outputs. Encoding will automatically run on the GPU if one is available.
 
@@ -141,12 +246,13 @@ def run_cli(
     encoder = SBERTEncoder(model_name)
 
     logger.info(f"Encoding text in batches of {batch_size}")
+
     text_by_document = [i[0] for i in text_and_ids]
     embs = encode_text(text_by_document, encoder, batch_size=batch_size)
 
     # Export embeddings to numpy memmap file
     embs_output_path = (
-        output_dir / f"embeddings_dim_{embs.shape[1]}_{model_name}_{curr_time}.memmap"
+            output_dir / f"embeddings_dim_{embs.shape[1]}_{model_name}_{curr_time}.memmap"
     )
     fp = np.memmap(embs_output_path, dtype="float32", mode="w+", shape=embs.shape)
     fp[:] = embs[:]
