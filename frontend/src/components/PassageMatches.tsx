79e43f33d709306fa9d7f19d40d6b6ae24d68c90
Paula Hightower <eurolife@gmail.com>
2022-05-02 13:01:08 +0100

    WIP display passage matches

--- frontend/src/components/PassageMatches.tsx
+++ frontend/src/components/PassageMatches.tsx
@@ -1,68 +1,43 @@
+import { useEffect } from 'react';
+import Loader from './Loader';
+
 const PassageMatches = ({ document }) => {
-  const { data } = document;
+  const { data: doc } = document;
+  useEffect(() => {});
   return (
-    <div>
-      {console.log(document)}
-      <h2>{data?.document_name}</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod
-        laoreet turpis. Morbi eu condimentum tellus. Curabitur rutrum varius
-        augue et ultrices. Proin sed auctor magna. Vivamus porta rutrum odio et
-        tempor. Fusce vitae tristique ex, nec elementum nibh. Phasellus id
-        blandit nisi.
-      </p>
+    <>
+      {!doc ? (
+        <div className="w-full flex justify-center h-96">
+          <Loader />
+        </div>
+      ) : (
+        <div>
+          {console.log(document.data)}
+          <div className="border-b border-blue-200 pb-4">
+            <h1 className="text-lg text-blue-500 font-medium px-4">
+              {doc.document_name}
+            </h1>
+            {/* TODO: translate below text, how to handle plurals? */}
+            <p className="px-4 text-indigo-500 text-sm">
+              {doc.document_passage_matches.length}{' '}
+              {`match${doc.document_passage_matches.length === 1 ? '' : 'es'}`}{' '}
+              in document.
+            </p>
+          </div>
 
-      <p>
-        Duis volutpat, augue non aliquet venenatis, sapien tortor volutpat enim,
-        eu aliquam justo sem quis augue. Pellentesque habitant morbi tristique
-        senectus et netus et malesuada fames ac turpis egestas. Aenean a nunc
-        faucibus, mattis nisl id, finibus nisl. Duis et sem at nibh varius
-        molestie et sed augue. Vivamus massa diam, tempor vel ex a, ornare
-        euismod urna. Proin nibh mauris, efficitur et mollis vel, volutpat id
-        ante. Sed sit amet nunc eu tortor placerat aliquam. Sed a consequat
-        magna, vel blandit erat.
-      </p>
-
-      <p>
-        Praesent tempor ut tortor ac mattis. Praesent scelerisque posuere nibh
-        non congue. Ut tempus interdum suscipit. Sed quis tellus nec ipsum
-        rutrum feugiat. Suspendisse potenti. Mauris dolor dui, fringilla eget
-        finibus a, pharetra feugiat magna. Donec egestas, sem nec mattis
-        placerat, sapien tellus interdum ligula, quis ultrices sapien tortor
-        vitae erat. Aenean porttitor bibendum mauris, sit amet faucibus nisi
-        efficitur et. Quisque et diam pellentesque, consequat urna ac, tristique
-        felis. Suspendisse vitae metus neque. Sed suscipit, tortor eu tempus
-        pharetra, elit leo gravida lacus, ut mollis nisi neque quis elit. Nam
-        rhoncus interdum quam, ac facilisis leo lacinia non. Maecenas lacinia
-        neque a sodales laoreet. Class aptent taciti sociosqu ad litora torquent
-        per conubia nostra, per inceptos himenaeos. Vivamus vestibulum, est sed
-        ultricies pharetra, orci magna venenatis neque, id condimentum nulla
-        turpis pulvinar neque.
-      </p>
-
-      <p>
-        Ut pretium fringilla ipsum, et ultrices elit faucibus non. Nam suscipit
-        vitae justo nec ullamcorper. Praesent cursus et quam a venenatis. Mauris
-        sit amet vehicula justo, quis consequat arcu. Nunc rutrum nisl et sapien
-        pretium, id laoreet eros accumsan. Duis aliquam at enim ut pharetra.
-        Vivamus sapien libero, aliquet non molestie ac, rutrum a dui. Integer mi
-        est, elementum id massa ut, pharetra pretium urna. Suspendisse aliquam,
-        mauris nec lacinia facilisis, nibh lacus consectetur felis, a semper
-        urna est dictum arcu. Lorem ipsum dolor sit amet, consectetur adipiscing
-        elit. Sed viverra, nulla non hendrerit condimentum, dui turpis rhoncus
-        neque, eu vulputate justo dui et justo. Vivamus sed dui vel neque semper
-        gravida eget et ipsum.
-      </p>
-      <p>
-        Nam eu euismod dolor. Cras varius viverra augue, at dictum libero ornare
-        viverra. Phasellus interdum aliquet volutpat. Cras a felis arcu. Integer
-        nisi nibh, commodo in ante non, ullamcorper imperdiet elit. Curabitur
-        non erat metus. Fusce nisi ligula, tincidunt eu orci ut, porttitor
-        accumsan dolor. Mauris sem sapien, facilisis a finibus vitae, auctor et
-        justo. Donec ut elit ipsum. Donec dictum, sapien et pharetra commodo,
-        velit lorem lacinia est, accumsan aliquet dui lorem quis enim.
-      </p>
-    </div>
+          <div className="px-4">
+            {doc.document_passage_matches.map((item) => (
+              <div key={item.text_block_id} className="py-4">
+                <span className="text-xs text-blue-500">
+                  On page {item.text_block_page}
+                </span>
+                <p className="mt-2 text-indigo-400">...{item.text}...</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 export default PassageMatches;
--- frontend/src/components/slideout/index.tsx
+++ frontend/src/components/slideout/index.tsx
@@ -10,13 +10,13 @@ const Slideout = ({ children, show, setShowSlideout }) => {
     <div
       className={`${
         show ? 'translate-x-0' : 'translate-x-150'
-      } transition duration-700 bg-white pt-12 z-50 shadow-xl h-screen fixed top-0 right-0 w-11/12 md:w-1/2 lg:w-5/12`}
+      } transition duration-700 bg-white pt-12 z-50 shadow-2xl shadow-indigo-500 h-screen fixed top-0 right-0 w-11/12 md:w-1/2 lg:w-5/12`}
     >
       <div className="absolute top-0 right-0 mt-4 mr-4">
         <Close onClick={() => setShowSlideout(false)} />
       </div>
 
-      <div className="px-4 h-full overflow-y-auto">{children}</div>
+      <div className="h-full overflow-y-auto">{children}</div>
     </div>
   );
 };
