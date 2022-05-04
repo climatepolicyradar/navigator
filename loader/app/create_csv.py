c121f26062dceeb96e3001d3912921dc2db53e23
Kalyan Dutia <kalyan.dutia@gmail.com>
2022-05-04 16:58:05 +0100

    Create script to transform google sheets excel export to csv

--- loader/app/create_csv.py
+++ loader/app/create_csv.py
@@ -2,8 +2,12 @@
 
 import os
 from pathlib import Path
-
+from html.parser import HTMLParser
+from io import StringIO
 import pandas as pd
+
+from app.loaders.loader_cclw_v2.extract.main import csv_column_map
+from app.loaders.loader_cclw_v1.extract.main import extract
 
 
 def get_data_dir():
@@ -89,36 +93,39 @@ def drop_missing_rows_from_merged_df(df_merged: pd.DataFrame) -> pd.DataFrame:
 def get_single_doc_actions_xlsx(single_doc_actions_path: Path) -> pd.DataFrame:
     return pd.read_excel(
         str(single_doc_actions_path), sheet_name="COMPLETED combined"
-    ).rename(columns={"Type": "Category"})
+    ).rename(columns={"Type": "Category", "Document Types": "Document Type"})
+
+
+class MLStripper(HTMLParser):
+    """Strips HTML from strings."""
+
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs = True
+        self.text = StringIO()
+
+    def handle_data(self, d):  # noqa:D102
+        self.text.write(d)
+
+    def get_data(self):  # noqa:D102
+        return self.text.getvalue()
+
+
+def strip_tags(html: str) -> str:
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
 
 
 if __name__ == "__main__":
     DATA_DIR = get_data_dir()
     MANUALLY_CREATED_DATA_PATH = DATA_DIR / "Master sheet completed.xlsx"
     SINGLE_DOC_ACTIONS_PATH = DATA_DIR / "Single doc action doc type fixes.xlsx"
+    CCLW_V1_CSV = DATA_DIR / "laws_and_policies_16022022.csv"
 
     single_doc_actions_df = get_single_doc_actions_xlsx(SINGLE_DOC_ACTIONS_PATH)
-
-    # From Juan loader
-    # TODO: can we import this from the loader code?
-    csv_column_map = {
-        "Id": "policy_id",  # for grouping related documents
-        "Title": "document_name",
-        "Geography ISO": "country_code",
-        "Documents": "document_url",  # column is plural, but it will be only one document URL
-        "Category": "policy_category",
-        # metadata
-        "Events": "events",
-        "Sectors": "sectors",
-        "Instruments": "instruments",
-        "Frameworks": "frameworks",
-        "Responses": "responses",
-        "Natural Hazards": "hazards",
-        "Document Type": "document_type",
-        "Year": "document_year",
-        "Language": "document_language",
-        "Keywords": "keywords",
-    }
 
     person_names = [
         "Callie",
@@ -133,7 +140,9 @@ if __name__ == "__main__":
         "Stef",
     ]
 
-    def process_data(df_documents, df_actions) -> pd.DataFrame:
+    def process_data(
+        df_documents, df_actions, df_cclw_v1_csv_with_single_url
+    ) -> pd.DataFrame:
         # Rows without a value for these columns will be dropped
         columns_required_values = ["Title"]
 
@@ -143,13 +152,39 @@ if __name__ == "__main__":
         df_merged = df_merged.rename(columns={"Type": "Category"})
 
         # 'Int64' is nullable int type, as some rows have no 'Year' value
-        df_merged.loc[:, ["Year", "Id"]] = df_merged.loc[:, ["Year", "Id"]].astype(
-            "Int64"
+        df_merged.loc[:, "Year"] = df_merged.loc[:, "Year"].astype("Int64")
+        df_merged.loc[:, "Id"] = df_merged.loc[:, "Id"].astype(int)
+
+        docs_and_actions_and_descriptions_from_v1_csv_merged = pd.merge(
+            left=df_merged,
+            right=df_cclw_v1_csv_with_single_url[["_url", "Description"]],
+            on="_url",
+        )
+        docs_and_actions_and_descriptions_from_v1_csv_merged = (
+            docs_and_actions_and_descriptions_from_v1_csv_merged[csv_column_map.keys()]
         )
 
-        return df_merged[csv_column_map.keys()]
+        return docs_and_actions_and_descriptions_from_v1_csv_merged
 
     data_list = []
+
+    # explode old CSV on URLs
+    df_cclw_v1_csv = extract(CCLW_V1_CSV)
+    df_cclw_v1_csv.dropna(subset=["document_list"], inplace=True)
+    df_cclw_v1_csv_with_single_url = pd.DataFrame()
+    for idx, row in df_cclw_v1_csv.iterrows():
+        doc_list = row["document_list"]
+        for parts in doc_list.split(";"):
+            url = parts.split("|")[1]
+            row["_url"] = url
+            row["policy_description"] = strip_tags(row["policy_description"])
+            df_cclw_v1_csv_with_single_url = pd.concat(
+                [df_cclw_v1_csv_with_single_url, row.to_frame().T]
+            )
+
+    df_cclw_v1_csv_with_single_url = df_cclw_v1_csv_with_single_url.rename(
+        columns={"policy_description": "Description"}
+    )
 
     for person in person_names:
         print(f"Processing {person}")
@@ -161,9 +196,12 @@ if __name__ == "__main__":
             str(MANUALLY_CREATED_DATA_PATH), sheet_name=f"{person} actions"
         )
 
-        data_list.append(process_data(df_documents, df_actions))
+        data_list.append(
+            process_data(df_documents, df_actions, df_cclw_v1_csv_with_single_url)
+        )
 
     all_manual_data = pd.concat(data_list, axis=0, ignore_index=True)
+
     data_final = pd.concat(
         [all_manual_data, single_doc_actions_df], axis=0, ignore_index=True
     )
