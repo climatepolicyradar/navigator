f226994efefa91c47cd978b69114acb460f48385
Paula Hightower <eurolife@gmail.com>
2022-05-17 17:18:48 +0100

    get rid of test code

--- frontend/src/hooks/useUpdateAccount.ts
+++ frontend/src/hooks/useUpdateAccount.ts
@@ -19,28 +19,3 @@ const useUpdateAccount = () => {
 };
 
 export default useUpdateAccount;
-
-// curl -X 'PUT' \
-//   'https://app.climatepolicyradar.org/api/v1/users/me' \
-//   -H 'accept: application/json' \
-//   -H 'Content-Type: application/json' \
-//   -H 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwYXVsYSthdXRoX3Rlc3RfNTlAY2xpbWF0ZXBvbGljeXJhZGFyLm9yZyIsInBlcm1pc3Npb25zIjoidXNlciIsImlzX2FjdGl2ZSI6dHJ1ZSwiZXhwIjoxNjUyODAzMDM2fQ.4SGkQuHnVLJrOCLX8dTXLaEjEJL-tJXskwZXHZ_rhCM' \
-//   -d '{
-//   "names": "Paula H",
-//   "job_role": "",
-//   "location": "UK",
-//   "affiliation_organisation": "x",
-//   "affiliation_type": [
-//     "string"
-//   ],
-//   "policy_type_of_interest": [
-//     "string"
-//   ],
-//   "geographies_of_interest": [
-//     "string"
-//   ],
-//   "data_focus_of_interest": [
-//     "string"
-//   ],
-//   "email": "paula+auth_test_59@climatepolicyradar.org"
-// }'
