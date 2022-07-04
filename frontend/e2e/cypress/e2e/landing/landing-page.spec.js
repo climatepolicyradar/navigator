2045c5d920afbebebf1375eca62bedec4354980a
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-07-04 14:01:51 +0100

    Extend wait to allow animation to finish

--- frontend/e2e/cypress/e2e/landing/landing-page.spec.js
+++ frontend/e2e/cypress/e2e/landing/landing-page.spec.js
@@ -9,7 +9,7 @@ describe('Landing page', () => {
 
   it('should see placeholder text which should clear after clicking input', () => {
     // wait for text animation to finish
-    cy.wait(2000);
+    cy.wait(3000);
 
     cy.get('[data-cy="search-input"]')
       .invoke('attr', 'placeholder')
