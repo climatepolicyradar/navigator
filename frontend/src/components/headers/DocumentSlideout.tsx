e80740c15193e541c9f1746310c6d757668f2ca7
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-07-11 17:23:21 +0100

    Add extra detail to matches slide out on search

--- frontend/src/components/headers/DocumentSlideout.tsx
+++ frontend/src/components/headers/DocumentSlideout.tsx
@@ -1,13 +1,18 @@
-import Link from 'next/link';
-import ToggleDocumentMenu from '../menus/ToggleDocumentMenu';
-import TextLink from '../nav/TextLink';
+import Link from "next/link";
+import ToggleDocumentMenu from "../menus/ToggleDocumentMenu";
+import TextLink from "../nav/TextLink";
 
-const DocumentSlideout = ({
-  document,
-  showPDF,
-  setShowPDF,
-  setPassageIndex,
-}) => {
+import { convertDate } from "@utils/timedate";
+
+const DocumentSlideout = ({ document, showPDF, setShowPDF, setPassageIndex }) => {
+  const formatDate = () => {
+    const eudate = document.document_date;
+    const dateArr = eudate.split("/");
+    return `${dateArr[1]}/${dateArr[0]}/${dateArr[2]}`;
+  };
+
+  const [year] = convertDate(formatDate());
+
   return (
     <>
       {document ? (
@@ -16,27 +21,22 @@ const DocumentSlideout = ({
             <div className="pl-6 pr-10 mt-2">
               <Link href={`/document/${document.document_id}`}>
                 <a>
-                  <h1 className="text-lg text-blue-500 font-medium">
-                    {document.document_name}
-                  </h1>
+                  <h1 className="text-lg text-blue-500 font-medium">{document.document_name}</h1>
                 </a>
               </Link>
+              <div className="flex flex-wrap lg:flex-nowrap text-sm text-indigo-400 my-2 items-center">
+                <div className={`rounded-sm border border-black flag-icon-background flag-icon-${document.document_country_code.toLowerCase()}`} />
+                <span className="ml-2">
+                  {document.document_country_english_shortname}, {year}
+                </span>
+              </div>
 
               {/* TODO: translate below text, how to handle plurals? */}
-              <p className="text-indigo-500 text-sm">
-                {document.document_passage_matches.length}{' '}
-                {`match${
-                  document.document_passage_matches.length === 1 ? '' : 'es'
-                }`}{' '}
-                in document.
-              </p>
+              <h3 className="text-indigo-500 text-xl">
+                Document {`match${document.document_passage_matches.length === 1 ? "" : "es"}`} ({document.document_passage_matches.length})
+              </h3>
             </div>
-            <ToggleDocumentMenu
-              setShowPDF={setShowPDF}
-              showPDF={showPDF}
-              document={document}
-              setPassageIndex={setPassageIndex}
-            />
+            <ToggleDocumentMenu setShowPDF={setShowPDF} showPDF={showPDF} document={document} setPassageIndex={setPassageIndex} />
           </div>
           {showPDF && (
             // TODO: translate below text
