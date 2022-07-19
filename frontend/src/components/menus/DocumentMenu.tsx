10f8d18fc41a9073d540b026d06a1ce6caa86ebc
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-07-19 16:09:38 +0100

    Mark first item of drawer menu

--- frontend/src/components/menus/DocumentMenu.tsx
+++ frontend/src/components/menus/DocumentMenu.tsx
@@ -7,7 +7,7 @@ const DocumentMenu = ({ document, setShowMenu }) => {
       {/* TODO: translate titles */}
       {document ? (
         <DropdownMenuWrapper setShowMenu={setShowMenu}>
-          <DropdownMenuItem href={`/pdf/${document.document_id}`} title="View PDF in full window" />
+          <DropdownMenuItem href={`/pdf/${document.document_id}`} title="View PDF in full window" first />
           <DropdownMenuItem href={document.document_url} title="Download PDF" />
           <DropdownMenuItem href={`/document/${document.document_id}`} title="View document details" />
         </DropdownMenuWrapper>
