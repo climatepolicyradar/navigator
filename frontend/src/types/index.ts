3c6069bff48a1749955de5407c7b5913227105f7
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-07-25 16:27:12 +0100

    Define dummy country and creating page

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -5,3 +5,34 @@ export type TSector = {
   parent_id?: number;
   source_id: number;
 };
+
+export type TCountry = {
+  name: string;
+  short_name: string;
+  continent: string;
+  legal_structure: string;
+  legal_bodies: string;
+  political_groups: string[];
+  financial_status: string;
+  gcri: number;
+  emissions: number;
+  laws: number;
+  policies: number;
+  cases: number;
+  events: TEvent[];
+  targets: TTarget[];
+};
+
+export type TTarget = {
+  target: string;
+  group: string;
+  base_year: string;
+  target_year: string;
+};
+
+type TEvent = {
+  name: string;
+  created_ts: string;
+  date: string;
+  description: string;
+};
