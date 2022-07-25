09eb8f834d0e4d3f5cd6028979136d34fa08fa38
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-07-25 17:55:23 +0100

    Add country header

--- frontend/src/pages/country/index.tsx
+++ frontend/src/pages/country/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from "React";
 import { TCountry, TTarget } from "@types";
 import Layout from "@components/layouts/Main";
+import Tooltip from "@components/tooltip";
 import { LawIcon, PolicyIcon, CaseIcon, TargetIcon } from "@components/svg/Icons";
 
 const SingleColumn: FC = ({ children }) => {
@@ -36,7 +37,7 @@ const Targets = ({ targets }: TTargets) => {
     <ul className="ml-4 list-disc list-outside">
       {targets.map((target) => (
         <li className="mb-4">
-          {target.target}
+          <span className="text-blue-700">{target.target}</span>
           <span className="block">
             <span className="font-semibold mr-1">{target.group}: ???</span>
             <span>
@@ -49,30 +50,67 @@ const Targets = ({ targets }: TTargets) => {
   );
 };
 
+type TCountryHeader = {
+  country: TCountry;
+};
+
+const CountryHeader = ({ country }: TCountryHeader) => {
+  const { short_name, continent, legal_structure, legal_bodies, political_groups, financial_status, gcri, emissions } = country;
+
+  return (
+    <div className="bg-offwhite border-solid border-blue-200 border-b py-6">
+      <div className="container">
+        <div className="md:max-w-lg">
+          <h1>{short_name}</h1>
+          <div className="grid grid-cols-2 gap-2 items-center">
+            <div className="font-semibold text-blue-700 text-xl">{continent}</div>
+            <div className="font-semibold text-blue-700 text-xl">
+              {legal_structure} ({legal_bodies})
+            </div>
+            <div>
+              <div className="text-blue-700 text-lg">Political groups</div>
+              <div className="font-semibold text-blue-700 text-xl">{political_groups.join(", ")}</div>
+            </div>
+            <div className="font-semibold text-blue-700 text-xl">{financial_status}</div>
+            <div>
+              <div className="text-blue-700 text-lg">Global Climate Risk Index</div>
+              <div className="font-semibold text-blue-700 text-xl flex">
+                <div className="mr-1">{gcri}</div> <Tooltip id="country-gcri" tooltip="Global Climate Risk Index" />
+              </div>
+            </div>
+            <div>
+              <div className="text-blue-700 text-lg">% Global Emissions</div>
+              <div className="font-semibold text-blue-700 text-xl">{emissions}%</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const CountryPage = () => {
   const country = COUNTRY;
   return (
     <Layout title={`Climate Policy Radar | ${country.name}`}>
       <section className="mb-8">
-        <div className="container">
-          {/* <CountryHeader /> */}
-          <SingleColumn>
-            <div className="grid grid-cols-2 md:grid-cols-3 gap-px rounded">
-              <KeyDetail detail="Laws" amount={country.laws} icon={<LawIcon />} />
-              <KeyDetail detail="Policies" amount={country.policies} icon={<PolicyIcon />} />
-              <KeyDetail detail="Cases" amount={country.cases} icon={<CaseIcon />} />
-            </div>
-            <div className="mt-8">
-              <h3 className="flex mb-4">
-                <span className="mr-2">
-                  <TargetIcon />
-                </span>
-                Targets ({country.targets.length})
-              </h3>
-              <Targets targets={country.targets} />
-            </div>
-          </SingleColumn>
-        </div>
+        <CountryHeader country={country} />
+        <SingleColumn>
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-px rounded">
+            <KeyDetail detail="Laws" amount={country.laws} icon={<LawIcon />} />
+            <KeyDetail detail="Policies" amount={country.policies} icon={<PolicyIcon />} />
+            <KeyDetail detail="Cases" amount={country.cases} icon={<CaseIcon />} />
+          </div>
+          <div className="mt-8">
+            <h3 className="flex mb-4">
+              <span className="mr-2">
+                <TargetIcon />
+              </span>
+              Targets ({country.targets.length})
+            </h3>
+            <Targets targets={country.targets} />
+          </div>
+        </SingleColumn>
       </section>
     </Layout>
   );
