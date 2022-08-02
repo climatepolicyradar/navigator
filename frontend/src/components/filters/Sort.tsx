2899de00545b10acb6f1aac2b9339cbec4197617
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-08-02 09:19:16 +0100

    Add sort options for browse functionality

--- frontend/src/components/filters/Sort.tsx
+++ frontend/src/components/filters/Sort.tsx
@@ -1,16 +1,18 @@
-import { sortOptions } from "@constants/sortOptions";
+import { sortOptions, sortOptionsBrowse } from "@constants/sortOptions";
 
 type TProps = {
   updateSort: (e: any) => void;
   defaultValue: string;
+  browse?: boolean;
 };
 
-const Sort = ({ updateSort, defaultValue }: TProps) => {
+const Sort = ({ updateSort, defaultValue, browse = false }: TProps) => {
+  const options = browse ? sortOptionsBrowse : sortOptions;
   return (
     <>
       <div className="flex-shrink-0 font-medium text-indigo-400">Sort by:</div>
       <select className="border border-indigo-200 small ml-2 z-0" onChange={updateSort} defaultValue={defaultValue}>
-        {sortOptions.map((item) => (
+        {options.map((item) => (
           <option key={item.value} value={item.value}>
             {item.label}
           </option>
