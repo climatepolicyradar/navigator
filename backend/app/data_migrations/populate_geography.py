1d454d0e25b2db02a89624d62f2dc896e5724b1f
diversemix <diversemix@gmail.com>
2022-08-04 15:57:27 +0100

    remove the intermeadiate dict

--- backend/app/data_migrations/populate_geography.py
+++ backend/app/data_migrations/populate_geography.py
@@ -8,37 +8,34 @@ def populate_geography(db):
     db session and 
     """
 
-    geographies = []
     # Get iso-3166 country codes. This file contains the standard iso-3166 codes + additional country codes for
     # regions that are missing - e.g. sub-saharan africa
     with open('alembic/versions/lookups/geography-iso-3166.csv', mode ='r') as file:
         # reading the CSV file
         csvFile = csv.DictReader(file)
-        geographies = [ l for l in csvFile ]
 
-    # Insert language codes into db table [=210]
-    for row in geographies:
+        for row in csvFile:
 
-        args = {
-            "display_value": row["World Bank Region"],
-            "value": row["World Bank Region"],
-            "type": "World Bank Region",
-        }
+            args = {
+                "display_value": row["World Bank Region"],
+                "value": row["World Bank Region"],
+                "type": "World Bank Region",
+            }
 
-        # Query for the parent already existing
-        geo_parent = db.query(Geography).filter_by(**args).first()
+            # Query for the parent already existing
+            geo_parent = db.query(Geography).filter_by(**args).first()
 
-        if not geo_parent:
-            # Create the parent
-            geo_parent = Geography(**args)
-            db.add(geo_parent)
-            db.flush()
+            if not geo_parent:
+                # Create the parent
+                geo_parent = Geography(**args)
+                db.add(geo_parent)
+                db.flush()
 
-        # Add the ISO-3166 Geography details
-        geo = Geography(
-            display_value=row["Name"],
-            value=row["Iso"],
-            type="ISO-3166",
-            parent_id=geo_parent.id,
-        )
-        db.add(geo)
+            # Add the ISO-3166 Geography details
+            geo = Geography(
+                display_value=row["Name"],
+                value=row["Iso"],
+                type="ISO-3166",
+                parent_id=geo_parent.id,
+            )
+            db.add(geo)
--- backend/app/data_migrations/populate_language.py
+++ backend/app/data_migrations/populate_language.py
@@ -20,17 +20,15 @@ def populate_language(db):
     """
     # Codes are obtained from: https://iso639-3.sil.org/sites/iso639-3/files/downloads/iso-639-3.tab
     # Get iso-630-3 codes from extracted file
-    languages = []
     with open('alembic/versions/lookups/language-iso-639-3.txt', mode='r') as file:
         # reading the CSV file
         csvFile = csv.DictReader(file, dialect=iso_csv)
-        languages = [l for l in csvFile]
 
-    for row in languages:
-        lang = {}
-        lang["language_code"] = row.pop("Id")
-        lang["part1_code"] = row.pop("Part1")
-        lang["part2_code"] = row.pop("Part2B")
-        lang["name"] = row.pop("Ref_Name")
+        for row in csvFile:
+            lang = {}
+            lang["language_code"] = row.pop("Id")
+            lang["part1_code"] = row.pop("Part1")
+            lang["part2_code"] = row.pop("Part2B")
+            lang["name"] = row.pop("Ref_Name")
 
-        db.add(Language(**lang))
+            db.add(Language(**lang))
