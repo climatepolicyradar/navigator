1eb2d07dc3e2412537903ae4defed2dbb8367d08
diversemix <diversemix@gmail.com>
2022-08-11 19:22:08 +0100

    use code in conftest

--- backend/tests/routes/test_geo_stats.py
+++ backend/tests/routes/test_geo_stats.py
@@ -1,20 +1,11 @@
-from fastapi.testclient import TestClient
-
 from unittest.mock import Mock
-from app.main import app
+from app.initial_data import populate_initial_data
 
 from app.api.api_v1.routers.geo_stats import (
     GeoStatsResponse,
     add_geo_stats_route,
     get_geo_stats,
 )
-from app.core.auth import get_current_active_user
-
-client = TestClient(app)
-
-
-async def override_current_user():
-    return "a test user"
 
 
 def test_adds_route():
@@ -23,20 +14,27 @@ def test_adds_route():
     router.add_api_route.assert_called_once()
 
 
-def test_endpoint_security():
+def test_endpoint_security(client):
     response = client.get("/api/v1/geo_stats/11")
     assert response.status_code == 401
 
 
-def test_endpoint_returns_correct_data():
-    app.dependency_overrides[get_current_active_user] = override_current_user
+def test_endpoint_returns_correct_data(client, user_token_headers, test_db):
+    populate_initial_data(test_db)
+    test_db.flush()  # update the session, no need to commit as its just a test
 
-    response = client.get("/api/v1/geo_stats/11")
+    TEST_ID = 11
+    TEST_GEO_NAME = "Antigua and Barbuda"
+
+    response = client.get(
+        f"/api/v1/geo_stats/{TEST_ID}",
+        headers=user_token_headers,
+    )
     stats = response.json()
     assert response.status_code == 200
     assert stats["id"] == 6
-    assert stats["geography_id"] == 11
-    assert stats["name"] == "Antigua and Barbuda"
+    assert stats["geography_id"] == {TEST_ID}
+    assert stats["name"] == TEST_GEO_NAME
 
 
 def test_queries_db():
