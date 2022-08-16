a0f5d02ebd93e9caa5d61ea3a5c3fc24076c9949
Peter Hooper <diversemix@gmail.com>
2022-08-16 10:35:17 +0100

    Check data does not exist before populating (#730)
    Check table before populating

--- backend/app/data_migrations/populate_language.py
+++ backend/app/data_migrations/populate_language.py
@@ -1,6 +1,7 @@
 import csv
 from app.db.models import Language
 from app.db.session import SessionLocal
+from .utils import has_rows
 
 
 # Create a new dialect - as its a bit wierd
@@ -20,6 +21,10 @@ csv.register_dialect("iso_csv", iso_csv)
 
 def populate_language(db: SessionLocal) -> None:
     """Populate languages from CSV file."""
+
+    if has_rows(db, Language):
+        return
+
     # Codes are obtained from: https://iso639-3.sil.org/sites/iso639-3/files/downloads/iso-639-3.tab
     # Get iso-630-3 codes from extracted file
     with open("app/data_migrations/data/language-iso-639-3.txt", mode="r") as file:
