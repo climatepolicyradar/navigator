9c76eb9a311ccb057ff081e657a0f26b334a5556
diversemix <diversemix@gmail.com>
2022-08-19 09:50:37 +0100

    add data to browseinformation

--- backend/app/api/api_v1/routers/search.py
+++ backend/app/api/api_v1/routers/search.py
@@ -10,6 +10,7 @@ from app.core.search import (
     OpenSearchQueryConfig,
 )
 from app.db.schemas.search import (
+    BrowseInformation,
     SearchRequestBody,
     SearchResponseBody,
 )
@@ -24,6 +25,12 @@ _OPENSEARCH_CONNECTION = OpenSearchConnection(opensearch_config=_OPENSEARCH_CONF
 _OPENSEARCH_INDEX_CONFIG = OpenSearchQueryConfig()
 
 
+def get_browse_info() -> BrowseInformation:
+    return BrowseInformation(
+        document_counts={}, top_documents={}, events=[], targets=[]
+    )
+
+
 @search_router.post("/searches", response_model=SearchResponseBody)
 def search_documents(
     request: Request,
@@ -36,8 +43,17 @@ def search_documents(
         extra={"props": {"search_request": json.loads(search_body.json())}},
     )
 
-    return _OPENSEARCH_CONNECTION.query(
-        search_request_body=search_body,
-        opensearch_internal_config=_OPENSEARCH_INDEX_CONFIG,
-        preference=str(current_user.id),
-    )
+    if search_body.query_string:
+        """When a query string is given - hand off the complete search to OpenSearch"""
+        response = _OPENSEARCH_CONNECTION.query(
+            search_request_body=search_body,
+            opensearch_internal_config=_OPENSEARCH_INDEX_CONFIG,
+            preference=str(current_user.id),
+        )
+    else:
+        """When no query string - search using RDS"""
+        response = SearchResponseBody(
+            hits=0, query_time_ms=0, browse_info=get_browse_info(), documents=[]
+        )
+
+    return response
