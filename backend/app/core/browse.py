3364ccfe108a15080eb7c033362c2f9292e38051
Peter Hooper <diversemix@gmail.com>
2022-08-22 10:44:10 +0100

    BAK-1147 - Add structure for using same "browse_rds" function for summaries page and unfocused search using RDS (#734)
    * add browse response
    
    * add data to browseinformation
    
    * try a union response
    
    * wip
    
    * Use a smart union
    
    * revert changes to SearchResponseBody
    
    * updates after chat with Joel
    
    * add geography_id to the country search
    
    * rename response for search by country
    
    * refactor to use summaries
    
    * correct the summary
    
    * remove vscode config
    
    * add FIXME
    
    * remove years from search_by_country
    
    * remove the settings for vscode
    
    * add checks for not pushing FIXMEs to main

---
+++ backend/app/core/browse.py
@@ -0,0 +1,12 @@
+"""Functions to support browsing the RDS document structure"""
+
+from app.db.schemas.search import (
+    SearchRequestBody,
+    SearchResponseBody,
+)
+
+
+def browse_rds(request: SearchRequestBody) -> SearchResponseBody:
+    """Broswe RDS"""
+    # FIXME: Implement this RDS search
+    return SearchResponseBody(hits=0, query_time_ms=0, documents=[])
