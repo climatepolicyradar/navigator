aa826ab9a5160801321a868b6210246a90e1a6dc
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-08-22 16:50:38 +0100

    Save search term to state when clicking country

--- frontend/src/components/forms/SearchDropdown.tsx
+++ frontend/src/components/forms/SearchDropdown.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import useUpdateSearchCriteria from "@hooks/useUpdateSearchCriteria";
 import useNestedLookups from "@hooks/useNestedLookups";
 import { TGeography } from "@types";
 import { SearchIcon } from "@components/svg/Icons";
@@ -10,6 +12,8 @@ type TProps = {
 };
 
 export const SearchDropdown = ({ show = false, term, handleSearchClick }: TProps) => {
+  const updateSearchCriteria = useUpdateSearchCriteria();
+  const router = useRouter();
   const geosQuery: any = useNestedLookups("geographies", "", 2);
   const countries: TGeography[] = geosQuery.data?.data?.level2 || [];
 
@@ -22,28 +26,36 @@ export const SearchDropdown = ({ show = false, term, handleSearchClick }: TProps
   const handleClick = (e: any) => {
     e.preventDefault();
     handleSearchClick();
-  }
+  };
 
-  const anchorClasses = (last: boolean) => `flex py-2 px-4 block hover:bg-blue-200 ${last ? "rounded-b-lg" : ""}`;
+  const handleCountryClick = (url: string) => {
+    updateSearchCriteria.mutate({ ["query_string"]: term });
+    router.push(url);
+  };
+
+  const anchorClasses = (last: boolean) => `flex items-center cursor-pointer py-2 px-4 block hover:bg-blue-200 ${last ? "rounded-b-lg" : ""}`;
 
   return (
     <div className="absolute top-[50px] bg-blue-100 w-full text-indigo-400 rounded-b-lg max-h-[300px] overflow-y-auto">
       <a href="#" className={anchorClasses(!countriesFiltered.length)} onClick={handleClick}>
-        <span className="mr-2 w-[20px]"><SearchIcon /></span>Search <span className="font-bold text-black mx-1">{term}</span> in all documents
+        <span className="mr-2 w-[20px]">
+          <SearchIcon />
+        </span>
+        Search <span className="font-bold text-black mx-1">{term}</span> in all documents
       </a>
       {!!countriesFiltered.length && (
         <>
           <div className="py-2 px-4 text-sm mt-2">View countries and territories information</div>
-          {countriesFiltered.map((country, i) => {
-            const last = i + 1 === countriesFiltered.length;
-            return (
-              <Link href={`/country/${country.id}`} key={country.id}>
-                <a className={anchorClasses(last)}>
+          <ul>
+            {countriesFiltered.map((country, i) => {
+              const last = i + 1 === countriesFiltered.length;
+              return (
+                <li className={anchorClasses(last)} key={country.id} onClick={() => handleCountryClick(`/country/${country.id}`)}>
                   <span className="font-bold text-black">{country.display_value}</span> <span className="text-sm ml-4">Country profile</span>
-                </a>
-              </Link>
-            );
-          })}
+                </li>
+              );
+            })}
+          </ul>
         </>
       )}
     </div>
