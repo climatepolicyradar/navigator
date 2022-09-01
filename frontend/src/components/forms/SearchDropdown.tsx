c699757955223c156433bd3b37d5297684367cb8
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-09-01 15:43:32 +0100

    Use semantic element to allow for tabbing through options

--- frontend/src/components/forms/SearchDropdown.tsx
+++ frontend/src/components/forms/SearchDropdown.tsx
@@ -15,10 +15,10 @@ export const SearchDropdown = ({ show = false, term, handleSearchClick }: TProps
   const updateSearchCriteria = useUpdateSearchCriteria();
   const router = useRouter();
   const geosQuery: any = useNestedLookups("geographies", "", 2);
-  const countries: TGeography[] = geosQuery.data?.data?.level2 || [];
+  const geographies: TGeography[] = geosQuery.data?.data?.level2 || [];
 
-  const countriesFiltered = countries.filter(
-    (country: TGeography) => country.display_value.toLowerCase().includes(term.toLocaleLowerCase()) || country.value.toLowerCase().includes(term.toLowerCase())
+  const geographiesFiltered = geographies.filter(
+    (geography: TGeography) => geography.display_value.toLowerCase().includes(term.toLocaleLowerCase()) || geography.value.toLowerCase().includes(term.toLowerCase())
   );
 
   if (!term || !show) return null;
@@ -28,30 +28,33 @@ export const SearchDropdown = ({ show = false, term, handleSearchClick }: TProps
     handleSearchClick();
   };
 
-  const handleCountryClick = (url: string) => {
+  const handleCountryClick = (e: React.MouseEvent<HTMLAnchorElement>, url: string) => {
+    e.preventDefault();
     updateSearchCriteria.mutate({ ["query_string"]: term });
     router.push(url);
   };
 
-  const anchorClasses = (last: boolean) => `flex items-center cursor-pointer py-2 px-4 block hover:bg-blue-200 ${last ? "rounded-b-lg" : ""}`;
+  const anchorClasses = (last: boolean) => `flex items-center cursor-pointer py-2 px-4 block hover:bg-blue-200 focus:bg-blue-200 ${last ? "rounded-b-lg" : ""}`;
 
   return (
     <div className="absolute top-[50px] bg-blue-100 w-full text-indigo-400 rounded-b-lg max-h-[300px] overflow-y-auto">
-      <a href="#" className={anchorClasses(!countriesFiltered.length)} onClick={handleClick}>
+      <a href="#" className={anchorClasses(!geographiesFiltered.length)} onClick={handleClick}>
         <span className="mr-2 w-[20px]">
           <SearchIcon />
         </span>
         Search <span className="font-bold text-black mx-1">{term}</span> in all documents
       </a>
-      {!!countriesFiltered.length && (
+      {!!geographiesFiltered.length && (
         <>
           <div className="py-2 px-4 text-sm mt-2">View countries and territories information</div>
           <ul>
-            {countriesFiltered.map((country, i) => {
-              const last = i + 1 === countriesFiltered.length;
+            {geographiesFiltered.map((geography, i) => {
+              const last = i + 1 === geographiesFiltered.length;
               return (
-                <li className={anchorClasses(last)} key={country.id} onClick={() => handleCountryClick(`/country/${country.id}`)}>
-                  <span className="font-bold text-black">{country.display_value}</span> <span className="text-sm ml-4">Country profile</span>
+                <li key={geography.id}>
+                  <a href="#" className={anchorClasses(last)} onClick={(e) => handleCountryClick(e, `/geography/${geography.id}`)}>
+                    <span className="font-bold text-black">{geography.display_value}</span> <span className="text-sm ml-4">Geography profile</span>
+                  </a>
                 </li>
               );
             })}
