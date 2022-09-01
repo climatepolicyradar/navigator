2b02636152ecddffc780bc2cfe53b8f897cc1dff
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-09-01 12:25:32 +0100

    Hide missing details

--- frontend/src/components/blocks/CountryHeader.tsx
+++ frontend/src/components/blocks/CountryHeader.tsx
@@ -26,32 +26,40 @@ export const CountryHeader = ({ country }: TProps) => {
           <h1 className="mb-6">{name}</h1>
           <div className="grid grid-cols-2 gap-6 items-center">
             <div className="font-semibold text-blue-700 text-xl">{getCountryRegion()}</div>
-            <div className="font-semibold text-blue-700 text-xl">
-              {federal && "Federative"} {federal && federal_details && <span className="font-light text-lg">({federal_details})</span>}
-            </div>
-            <div>
-              <div className="text-blue-700 text-lg">Political Groups</div>
-              <div className="font-semibold text-blue-700 text-xl">{political_groups.split(";").join(", ")}</div>
-            </div>
-            <div>
-              <div className="text-blue-700 text-lg">World Bank Income Group</div>
-              <div className="font-semibold text-blue-700 text-xl">{worldbank_income_group}</div>
-            </div>
-            <div>
-              <div className="text-blue-700 text-lg">Global Climate Risk Index</div>
-              <div className="font-semibold text-blue-700 text-xl flex">
-                <div className="mr-1">{climate_risk_index}</div>{" "}
-                <Tooltip
-                  id="country-gcri"
-                  tooltip="The annually published Global Climate Risk Index analyses to what extent countries have been affected by the impacts of weather-related loss events (storms, floods, heat waves etc.)."
-                  icon="i"
-                />
+            {federal && (
+              <div className="font-semibold text-blue-700 text-xl">Federative {federal && federal_details && <span className="font-light text-lg">({federal_details})</span>}</div>
+            )}
+            {political_groups !== "" && (
+              <div>
+                <div className="text-blue-700 text-lg">Political Groups</div>
+                <div className="font-semibold text-blue-700 text-xl">{political_groups.split(";").join(", ")}</div>
               </div>
-            </div>
-            <div>
-              <div className="text-blue-700 text-lg">Share of Global Emissions</div>
-              <div className="font-semibold text-blue-700 text-xl">{global_emissions_percent}%</div>
-            </div>
+            )}
+            {worldbank_income_group !== "" && (
+              <div>
+                <div className="text-blue-700 text-lg">World Bank Income Group</div>
+                <div className="font-semibold text-blue-700 text-xl">{worldbank_income_group}</div>
+              </div>
+            )}
+            {climate_risk_index !== null && (
+              <div>
+                <div className="text-blue-700 text-lg">Global Climate Risk Index</div>
+                <div className="font-semibold text-blue-700 text-xl flex">
+                  <div className="mr-1">{climate_risk_index}</div>{" "}
+                  <Tooltip
+                    id="country-gcri"
+                    tooltip="The annually published Global Climate Risk Index analyses to what extent countries have been affected by the impacts of weather-related loss events (storms, floods, heat waves etc.)."
+                    icon="i"
+                  />
+                </div>
+              </div>
+            )}
+            {global_emissions_percent !== null && (
+              <div>
+                <div className="text-blue-700 text-lg">Share of Global Emissions</div>
+                <div className="font-semibold text-blue-700 text-xl">{global_emissions_percent}%</div>
+              </div>
+            )}
           </div>
         </div>
         {countryGeography?.value && (
