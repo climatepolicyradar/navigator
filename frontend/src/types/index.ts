7459b805acb704ce48fd2b8afd986f45ebf6c663
Joel Wright <joel@climatepolicyradar.org>
2022-09-06 09:40:31 +0100

    Enhanced Search Bar
    Detect country names in search and allow
     * search for <terms> in <geography>
     * access <geography> cover page

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -70,6 +70,12 @@ export type TGeographyStats = {
   climate_risk_index: number;
   worldbank_income_group: string;
   visibility_status: string;
+  laws?: number;
+  policies?: number;
+  cases?: number;
+  events?: TEvent[];
+  targets?: TTarget[];
+  documents?: TAssociatedDocument[];
 };
 
 export type TGeographySummary = {
