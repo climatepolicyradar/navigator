765c88f3f93aa4f643c329ffc69cf8ed6f17f8c5
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-09-13 14:39:28 +0100

    Make site header to work without auth

--- frontend/src/components/menus/AccountMenu.tsx
+++ frontend/src/components/menus/AccountMenu.tsx
@@ -1,7 +1,7 @@
 import DropdownMenuItem from "./DropdownMenuItem";
 import DropdownMenuWrapper from "./DropdownMenuWrapper";
 
-const AccountMenu = ({ setShowMenu, logout, user = null }) => {
+const AccountMenu = ({ setShowMenu, logout, user }) => {
   return (
     <DropdownMenuWrapper>
       <DropdownMenuItem href="https://climatepolicyradar.org" title="About us" target="_blank" first={true} setShowMenu={setShowMenu} />
--- frontend/src/components/menus/ToggleAccountMenu.tsx
+++ frontend/src/components/menus/ToggleAccountMenu.tsx
@@ -1,16 +1,18 @@
-import { useState, useRef } from 'react';
-import AccountMenu from '../menus/AccountMenu';
-import useOutsideAlerter from '../../hooks/useOutsideAlerter';
-import { MenuIcon } from '../svg/Icons';
+import { useState, useRef } from "react";
+import useOutsideAlerter from "@hooks/useOutsideAlerter";
+import AccountMenu from "../menus/AccountMenu";
+import { MenuIcon } from "../svg/Icons";
 
 const ToggleAccountMenu = ({ logout, user }) => {
   const [showMenu, setShowMenu] = useState(false);
   const menuRef = useRef(null);
   useOutsideAlerter(menuRef, () => setShowMenu(false));
+
   const toggleMenu = (e) => {
     e.preventDefault();
     setShowMenu(!showMenu);
   };
+
   return (
     <div ref={menuRef} className="ml-auto relative z-20">
       <button data-cy="menu-icon" onClick={toggleMenu}>
