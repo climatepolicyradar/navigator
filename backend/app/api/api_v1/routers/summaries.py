74ccf9bff1328dc939906bbfef6accda1363ff78
Joel Wright <joel@climatepolicyradar.org>
2022-09-14 09:45:26 +0100

    Bugfix: BAK-1161 Remove auth requirement from search/summary endpoints (#785)
    Remove auth requirement from search/summary endpoints

--- backend/app/api/api_v1/routers/summaries.py
+++ backend/app/api/api_v1/routers/summaries.py
@@ -4,7 +4,6 @@ Like searches but with pre-defined results based on the summary context.
 """
 import logging
 from fastapi import APIRouter, Depends, Request
-from app.core.auth import get_current_active_db_user
 from app.api.api_v1.schemas.search import (
     SummaryCountryResponse,
     CategoryName,
@@ -30,7 +29,6 @@ summary_router = APIRouter()
 def search_by_country(
     request: Request,
     geography_id: int,
-    current_user=Depends(get_current_active_db_user),
     db=Depends(get_db),
 ):
     """Searches the documents filtering by country and grouping by category."""
--- backend/tests/routes/test_summaries_country.py
+++ backend/tests/routes/test_summaries_country.py
@@ -1,13 +1,8 @@
-from http.client import OK, UNAUTHORIZED
+from http.client import OK
 
 
 def URL_UNDER_TEST(id: int) -> str:
     return f"/api/v1/summaries/country/{id}"
-
-
-def test_endpoint_security(client):
-    response = client.get(URL_UNDER_TEST(99))
-    assert response.status_code == UNAUTHORIZED
 
 
 def test_endpoint_returns_ok(client, user_token_headers):
@@ -53,14 +48,11 @@ def test_geography_with_documents(client, user_token_headers, summary_country_da
     assert len(resp["targets"]) == 0
 
 
-def test_geography_with_documents_ordered(
-    client, user_token_headers, summary_country_data
-):
+def test_geography_with_documents_ordered(client, summary_country_data):
     """Test that all the data is returned ordered by published date"""
     geography_id = summary_country_data["geos"][0].id
     response = client.get(
         URL_UNDER_TEST(geography_id),
-        headers=user_token_headers,
     )
     assert response.status_code == OK
     resp = response.json()
