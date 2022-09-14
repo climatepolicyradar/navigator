69cadee0310979c1584e719b21b19e7c75035203
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-09-14 11:51:54 +0100

    Remove reference to useAuth

--- frontend/src/components/headers/Main.tsx
+++ frontend/src/components/headers/Main.tsx
@@ -1,10 +1,7 @@
-import { useAuth } from "@api/auth";
 import ToggleAccountMenu from "../menus/ToggleAccountMenu";
 import AlphaLogoSmall from "../logo/AlphaLogoSmall";
 
 const Header = () => {
-  const { user, logout } = useAuth();
-
   return (
     <header data-cy="header" className="absolute bg-transparent w-full top-0 left-0 transition duration-300 z-10">
       <div className="container my-4">
@@ -12,7 +9,7 @@ const Header = () => {
           <AlphaLogoSmall />
 
           <div>
-            <ToggleAccountMenu logout={logout} user={user} />
+            <ToggleAccountMenu />
           </div>
         </div>
       </div>
