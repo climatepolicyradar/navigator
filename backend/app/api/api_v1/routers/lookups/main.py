c2461bc99af051d5d181c8d4588fcdd286e7f18d
Joel Wright <joel@climatepolicyradar.org>
2022-09-28 18:06:18 +0100

    Add all metadata values to config endpoint (#799)
    * Add all metadata values to config endpoint
    
    * Tests for additional metadata lookups
    
    * Update test comparison for collections
    
    * Carry changes through to response model

--- backend/app/api/api_v1/routers/lookups/main.py
+++ backend/app/api/api_v1/routers/lookups/main.py
@@ -1,13 +1,17 @@
 from fastapi import Depends, Request
 
 from app.db.models import (
-    Geography,
-    Language,
-    Source,
-    Instrument,
-    Sector,
-    DocumentType,
     Category,
+    DocumentType,
+    Framework,
+    Geography,
+    Hazard,
+    Instrument,
+    Keyword,
+    Language,
+    Response,
+    Sector,
+    Source,
 )
 from app.api.api_v1.schemas.metadata import Config
 from app.db.session import get_db
@@ -57,10 +61,17 @@ def lookup_config(
 ):
     """Get the config for the metadata."""
     cclw_source_collection = {
-        "geographies": tree_table_to_json(table=Geography, db=db),
+        "categories": table_to_json(table=Category, db=db),
         "document_types": table_to_json(table=DocumentType, db=db),
-        "sectors": tree_table_to_json(table=Sector, db=db),
+        "frameworks": table_to_json(table=Framework, db=db),
+        "geographies": tree_table_to_json(table=Geography, db=db),
+        "hazards": table_to_json(table=Hazard, db=db),
         "instruments": tree_table_to_json(table=Instrument, db=db),
+        "keywords": table_to_json(table=Keyword, db=db),
+        "languages": table_to_json(table=Language, db=db),
+        "sectors": tree_table_to_json(table=Sector, db=db),
+        "sources": table_to_json(table=Source, db=db),
+        "topics": table_to_json(table=Response, db=db),
     }
 
     source_collections = {"CCLW": cclw_source_collection}
