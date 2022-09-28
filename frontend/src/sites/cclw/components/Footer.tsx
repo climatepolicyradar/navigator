58d9ed301c58475edbb9a0bccdb8f924d6346c62
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-09-28 17:22:45 +0100

    Add footer shell

--- frontend/src/sites/cclw/components/Footer.tsx
+++ frontend/src/sites/cclw/components/Footer.tsx
@@ -1,10 +1,15 @@
 const Footer = () => {
   return (
-    <footer className="py-12 bg-blue-500 flex items-center shrink-0">
-      <div className="container">
-        <p className="font-medium text-lg mb-6 text-white md:text-center">
-          CCLW footer
-        </p>
+    <footer className="flex flex-col">
+      <div className="py-12 bg-grey-400">
+        <div className="container">
+          <p className="font-medium text-lg mb-6">upper footer</p>
+        </div>
+      </div>
+      <div className="py-12 bg-grey-500">
+        <div className="container">
+          <p className="font-medium text-lg mb-6">lower footer</p>
+        </div>
       </div>
     </footer>
   );
