d2680b650e4b530dd5a7c5a809a1a203b8a7f130
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-09-30 16:47:54 +0100

    Add colour adjustments for CCLW

--- frontend/src/components/dividers/Divider.tsx
+++ frontend/src/components/dividers/Divider.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 export const Divider: FC = ({ children }) => {
   return (
     <div className="relative">
-      <div className="bg-blue-600 w-full h-px absolute top-1/2 z-0"></div>
+      <div className="bg-secondary-700 w-full h-px absolute top-1/2 z-0"></div>
       <div className="flex justify-center relative z-10">{children}</div>
     </div>
   );
