51fea57f406ac20633fc7d83fcd6d1901be9ef4a
diversemix <diversemix@gmail.com>
2022-10-04 15:32:29 +0100

    Remove noqa F401

--- backend/app/db/models/__init__.py
+++ backend/app/db/models/__init__.py
@@ -24,9 +24,9 @@ These lookups are source-specific:
 - sector
 """
 
-from .document import *  # noqa F401 F403
-from .user import *  # noqa F401 F403
-from .source import *  # noqa F401 F403
-from .geography import *  # noqa F401 F403
-from .case import *  # noqa F401 F403
-from .litiguation import *  # noqa F401 F403
+from .document import *  # noqa F403
+from .user import *  # noqa F403
+from .source import *  # noqa F403
+from .geography import *  # noqa F403
+from .case import *  # noqa F403
+from .litiguation import *  # noqa F403
