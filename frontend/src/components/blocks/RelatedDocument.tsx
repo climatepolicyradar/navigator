1507bb9d3ecf438339bd84e5821cfd53659333f0
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-10-04 11:11:11 +0100

    Refactor document list item into own component

--- frontend/src/components/blocks/RelatedDocument.tsx
+++ frontend/src/components/blocks/RelatedDocument.tsx
@@ -1,9 +1,6 @@
-import Link from "next/link";
-import { truncateString } from "../../helpers";
-import { getCategoryIcon } from "@helpers/getCatgeoryIcon";
 import { convertDate } from "@utils/timedate";
 import { TAssociatedDocument } from "@types";
-import { CountryLink } from "@components/CountryLink";
+import { DocumentListItem } from "@components/document/DocumentListItem";
 
 interface RelatedDocumentProps {
   document: TAssociatedDocument;
@@ -12,24 +9,18 @@ interface RelatedDocumentProps {
 export const RelatedDocument = ({ document }: RelatedDocumentProps) => {
   const { country_code, country_name, document_id, publication_ts, category, description, name } = document;
   const [year] = convertDate(publication_ts);
+
   return (
-    <div className="relative">
-      <div className="flex justify-between items-start">
-        <h2 className="leading-none flex items-start">
-          <Link href={`/document/${document_id}`}>
-            <a className="text-left text-blue-500 font-medium text-lg transition duration-300 hover:text-indigo-600 leading-tight underline">{truncateString(name, 80)}</a>
-          </Link>
-        </h2>
-      </div>
-      <div className="flex text-sm text-indigo-400 mt-3 font-medium">
-        {category && <div className="mr-3">{getCategoryIcon(category, "20")}</div>}
-        <CountryLink countryCode={country_code}>
-          <div className={`rounded-sm border border-black flag-icon-background flag-icon-${country_code.toLowerCase()}`} />
-          <span className="ml-2">{country_name}</span>
-        </CountryLink>
-        <span>, {year}</span>
-      </div>
-      <p className="text-indigo-400 mt-3 text-content">{truncateString(description.replace(/(<([^>]+)>)/gi, ""), 250)}</p>
-    </div>
+    <DocumentListItem
+      listItem={{
+        id: document_id,
+        name: name,
+        country_code: country_code,
+        country_name: country_name,
+        document_year: year,
+        description: description,
+        category: category,
+      }}
+    />
   );
 };
---
+++ frontend/src/components/document/DocumentListItem.tsx
@@ -0,0 +1,52 @@
+import { FC } from "react";
+import Link from "next/link";
+import { TEventCategory } from "@types";
+import { truncateString } from "@helpers/index";
+import { getCategoryIcon } from "@helpers/getCatgeoryIcon";
+import { CountryLink } from "@components/CountryLink";
+import getSite from "@utils/getSite";
+
+type TProps = {
+  listItem: {
+    id: number;
+    country_code: string;
+    country_name: string;
+    description: string;
+    name: string;
+    document_year: string;
+    category?: TEventCategory;
+  };
+};
+
+export const DocumentListItem: FC<TProps> = ({ children, listItem }) => {
+  const { id, country_code, country_name, description, name, document_year, category } = listItem;
+  const site = getSite();
+
+  return (
+    <div className="relative">
+      <div className="flex justify-between items-start">
+        <h2 className="leading-none flex items-start">
+          <Link href={`/document/${id}`}>
+            <a className={`text-left text-blue-500 font-medium text-lg transition duration-300 leading-tight hover:underline ${site === "cpr" ? "underline" : ""}`}>
+              {truncateString(name, 80)}
+            </a>
+          </Link>
+        </h2>
+      </div>
+      <div className="flex flex-wrap text-sm text-indigo-400 mt-4 items-center space-y-1.5 font-medium">
+        {category && (
+          <div className="mr-3" title={category}>
+            {getCategoryIcon(category, "20")}
+          </div>
+        )}
+        <CountryLink countryCode={country_code}>
+          <div className={`rounded-sm border border-black flag-icon-background flag-icon-${country_code.toLowerCase()}`} />
+          <span className="ml-2">{country_name}</span>
+        </CountryLink>
+        <span>, {document_year}</span>
+        {children}
+      </div>
+      <p className="text-indigo-400 mt-3 text-content">{truncateString(description.replace(/(<([^>]+)>)/gi, ""), 375)}</p>
+    </div>
+  );
+};
