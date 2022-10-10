11162eea45f88660066c26491ace5f98c79459c0
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-10-10 14:02:51 +0100

    Feature/google search console (#808)
    * Add google search console validation file
    * Add dynamic robots.txt generation

---
+++ frontend/src/pages/robots.txt.ts
@@ -0,0 +1,21 @@
+const crawlableRobotsTxt = `User-agent: *\nAllow: /`;
+
+const uncrawlableRobotsTxt = `User-agent: *\nDisallow: /`;
+
+const isProd = () => {
+  return process.env.NEXT_PUBLIC_API_URL && !process.env.NEXT_PUBLIC_API_URL.startsWith("https://dev");
+};
+
+function Robots() {}
+
+export async function getServerSideProps({ res }) {
+  res.setHeader("Content-Type", "text/plan");
+  res.write(isProd() ? crawlableRobotsTxt : uncrawlableRobotsTxt);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default Robots;
