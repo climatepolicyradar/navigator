9fd65a475911ddace3643734c56e3c959be463f3
Peter Hooper <diversemix@gmail.com>
2022-10-14 08:42:26 +0100

    feature/FRO 110/generate sitemap (#818)
    Sitemap generated just for the geography pages for proof of concept

---
+++ frontend/src/pages/sitemap.txt.ts
@@ -0,0 +1,40 @@
+import { ApiClient } from "@api/http-common";
+import { TGeographyConfig } from "@types";
+
+function Sitemap() { }
+
+function extractGeographyIds(config: TGeographyConfig): number[] {
+  const children_ids: number[] = config.children.flatMap((node): number[] => (extractGeographyIds(node)));
+  return [config.node.id].concat(children_ids);
+}
+
+async function fetchGeographies(): Promise<TGeographyConfig[]> {
+  const client = new ApiClient();
+  const { data: data } = await client.get(`/config/`, null);
+  return data.metadata.CCLW.geographies;
+}
+
+async function getGeographyIds(): Promise<number[]> {
+  const geographyData: TGeographyConfig[] = await fetchGeographies();
+  return geographyData.flatMap((item: TGeographyConfig) => extractGeographyIds(item));
+}
+
+async function getGeographyPages(res: any): Promise<string[]> {
+  const host = res.req.headers.host;
+  const id_list: number[] = await getGeographyIds();
+  return id_list.map((geo_id: number) => `https://${host}/geographies/${geo_id}`);
+}
+
+export async function getServerSideProps({ res }) {
+
+  res.setHeader("Content-Type", "text/plain");
+  res.write((await getGeographyPages(res)).join('\n'));
+  res.end();
+
+  return {
+    props: {},
+  };
+
+}
+
+export default Sitemap;
