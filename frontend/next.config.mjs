e7225cf2859edd2dbb9b4151fc3151f3ae83e023
Peter Hooper <diversemix@gmail.com>
2022-10-21 09:16:57 +0100

    FRO-117: Redirect solution for preserving CCLW SEO (#825)
    Redirect solution using CSV file(s) configured with Env var

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,18 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+
+import read from "./redirects/reader.mjs"
+const REDIRECT_FILE = process.env.NEXT_REDIRECT_FILE || "default.csv"
+
+const nextConfig = {
   i18n: {
     locales: ["en", "fr"],
     defaultLocale: "en",
   },
   pageExtensions: ["tsx", "ts"],
   async redirects() {
-    return [
+    const standardRedirects = [
       {
         source: '/auth/:id*',
         destination: '/',
@@ -26,6 +33,10 @@ module.exports = {
         destination: '/',
         permanent: true,
       },
-    ]
+    ];
+
+    return standardRedirects.concat(await read(REDIRECT_FILE));
   },
-};
+}
+
+export default nextConfig
