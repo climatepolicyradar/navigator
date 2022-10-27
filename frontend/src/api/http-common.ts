254ecbacd4af8b20bf49f68e12ad01791c6ce0e9
Peter Hooper <diversemix@gmail.com>
2022-10-27 12:18:00 +0100

    peter branch (#832)
    some logging

--- frontend/src/api/http-common.ts
+++ frontend/src/api/http-common.ts
@@ -11,18 +11,22 @@ class ApiClient {
       this.baseUrl = process.env.NEXT_PUBLIC_API_URL;
     }
     this.axiosClient = axios.create();
+    console.log(`APIClient created to: ${this.baseUrl}` );
   }
 
   /**
    * Submit a GET request and return the response as a mapped promise.
    */
   get(url: string, params?: any) {
+    console.log(`GET: ${this.baseUrl}${url}`);
     return this.axiosClient
       .get(`${this.baseUrl}${url}`, { params })
       .then((res: any) => res)
       .catch((err: any) => Promise.reject(err));
   }
+
   post(url: string, values: any, config = {}) {
+    console.log(`POST: ${this.baseUrl}${url}`);
     return this.axiosClient
       .post(`${this.baseUrl}${url}`, values, config)
       .then((res) => res)
@@ -31,7 +35,10 @@ class ApiClient {
         return Promise.reject(err);
       });
   }
+  
   put(url: string, values: any) {
+    console.log(`PUT: ${this.baseUrl}${url}`);
+
     return this.axiosClient
       .put(`${this.baseUrl}${url}`, values)
       .then((res) => res)
