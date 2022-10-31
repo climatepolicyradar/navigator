5705c5717e401b09631946557000b3f03e9a7e9c
diversemix <diversemix@gmail.com>
2022-10-31 19:02:01 +0000

    clean up

--- backend/app/initial_data.py
+++ backend/app/initial_data.py
@@ -72,27 +72,9 @@ def create_superuser(db) -> None:
         )
 
 
-def create_loader_machine_user(db) -> None:
-    machineuser_email = os.getenv("MACHINE_USER_LOADER_EMAIL")
-    try:
-
-        create_user(
-            db,
-            machineuser_email,
-            os.getenv("MACHINE_USER_LOADER_PASSWORD"),
-        )
-    except IntegrityError:
-        print(
-            f"Skipping - loader machine user already exists with email/username {machineuser_email}"
-        )
-
-
 def populate_initial_data(db):
     print("Creating superuser...")
     create_superuser(db)
-
-    print("Creating loader machine user...")
-    create_loader_machine_user(db)
 
     print("Running data migrations...")
     run_data_migrations(db)
