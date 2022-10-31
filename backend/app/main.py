bb7f85ec0f155a4557a6a6bd2ecd049c4525eff3
diversemix <diversemix@gmail.com>
2022-10-31 11:52:30 +0000

    add as wildcards

--- backend/app/main.py
+++ backend/app/main.py
@@ -76,14 +76,13 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://app.climatepolicyradar.org",
-        "https://cclw.climatepolicyradar.org",
-        "https://app.dev.climatepolicyradar.org",
-        "https://cclw.dev.climatepolicyradar.org",
+        r"https:\/\/.*\.climatepolicyradar\.org",
+        r"https:\/\/.*\.dev\.climatepolicyradar\.org",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_origin_regex=True,
 )
 
 # add health endpoint
