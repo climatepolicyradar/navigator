4d12c30480bc1826e07ca03657c47d52fd9333a3
diversemix <diversemix@gmail.com>
2022-11-07 20:44:46 +0000

    finish script

--- backend/scripts/import_doc_relationships/import_doc_relationships.py
+++ backend/scripts/import_doc_relationships/import_doc_relationships.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
+from datetime import datetime
+from app.db.models.document import DocumentRelationship, Relationship
 from app.db.session import SessionLocal
+
+
+def create_relationship_with_docs(db, name, document_ids):
+    print(f"Creating relationship '{name}' for ids: {document_ids}...")
+
+    relationship = Relationship(
+        name=name, type="SYSTEM", description=f"imported at {datetime.now()}"
+    )
+    db.add(relationship)
+    db.flush()
+
+    for doc_id in document_ids:
+        link = DocumentRelationship(document_id=doc_id, relationship_id=relationship.id)
+        db.add(link)
+        print(f"... added {doc_id} to relationship {relationship.id}")
+
 
 if __name__ == "__main__":
     print("Importing document relationships...")
@@ -15,10 +33,13 @@ if __name__ == "__main__":
     """
     ).all()
 
-    for group in groups:
-        name = f"imported_relation_{group[0]}"
-        document_ids = group[1]
-        print(f"Creating relationship '{name}' for ids: {document_ids}")
-
-    # Do the biz
-    db.commit()
+    try:
+        with db.begin_nested():
+            for group in groups:
+                create_relationship_with_docs(
+                    db, f"imported_relation_{group[0]}", group[1]
+                )
+        db.commit()
+    except Exception as e:
+        print(e)
+        print("Error - nothing committed to the database")
