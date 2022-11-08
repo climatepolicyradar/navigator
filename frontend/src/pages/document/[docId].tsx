d60fb6f2a02733f85d192633021925457904b560
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-11-08 16:29:29 +0000

    Update GRI terms of use link

--- frontend/src/pages/document/[docId].tsx
+++ frontend/src/pages/document/[docId].tsx
@@ -163,7 +163,9 @@ const DocumentCoverPage: InferGetServerSidePropsType<typeof getServerSideProps> 
                     <ExternalLink url="http://lse.ac.uk/grantham" className="text-blue-500 hover:text-indigo-600 hover:underline transition duration-300">
                       Grantham Research Institute
                     </ExternalLink>
-                    . If you want to use this summary, please check Terms of use for citation and licensing of third party data.
+                    . If you want to use this summary, please check{" "}
+                    <ExternalLink url="https://www.lse.ac.uk/granthaminstitute/cclw-terms-and-conditions"  className="text-blue-500 hover:text-indigo-600 hover:underline transition duration-300">terms of use</ExternalLink> for citation and licensing of third party
+                    data.
                   </p>
                 </div>
               </div>
