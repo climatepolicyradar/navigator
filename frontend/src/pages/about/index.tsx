8b2f64fd58ccc01f95957436550130b971d8dc2b
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-11-08 11:50:01 +0000

    Copy update

--- frontend/src/pages/about/index.tsx
+++ frontend/src/pages/about/index.tsx
@@ -29,8 +29,8 @@ const About = () => {
           <h2>Invitation to contribute</h2>
           <p>
             We aim for the datasets to be as comprehensive and accurate as possible. However, there is no claim to have identified every relevant law, policy or court case in the
-            countries covered. We invite anyone to draw our attention to any information we may have missed or any errors or updates to existing data. Please follow{" "}
-            <ExternalLink url="https://docs.google.com/forms/d/e/1FAIpQLSdBdX2mLzFdeP8sTd_K7dsv8oylX6EBQucR_HhiXYZm1GA6gg/viewform">this link</ExternalLink> or email{" "}
+            countries covered. We invite anyone to draw our attention to any information we may have missed or any errors or updates to existing data. Please{" "}
+            <ExternalLink url="https://docs.google.com/forms/d/e/1FAIpQLSdBdX2mLzFdeP8sTd_K7dsv8oylX6EBQucR_HhiXYZm1GA6gg/viewform">fill out our form</ExternalLink> or email{" "}
             <ExternalLink url="mailto:gri.cgl@lse.co.uk">gri.cgl@lse.ac.uk</ExternalLink> to contribute.
           </p>
         </div>
