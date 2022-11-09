8865845ebfde6d12d2e4f6dcb36886758ad0aaf4
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-11-09 17:18:47 +0000

    update page title for CCLW

--- frontend/src/utils/getPageTitle.ts
+++ frontend/src/utils/getPageTitle.ts
@@ -2,7 +2,7 @@ export default function getPageTitle(site: string): string {
   let title = "Climate Policy Radar";
   switch (site) {
     case "cclw":
-      title = "Climate Laws of the World";
+      title = "Climate Change Laws of the World";
       break;
   }
   return title;
