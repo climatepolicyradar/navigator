6d194af4e2b3a93c8db30e89c3c56da5e1e6f5b8
diversemix <diversemix@gmail.com>
2022-11-29 20:25:54 +0000

    get redirects working

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,49 @@
-/*
-  THIS IS BURNED AT BUILDTIME - ENV ARE IGNORED AT RUNTIME
+// for pages that are not in cclw's sitemap
+const cclwRedirects = [
+    { source: "/cookie-policy", destination: "/", permanent: true },
+    { source: "/privacy", destination: "/", permanent: true },
+];
 
-  NOTE: If you are writing code in here beyond setting vars...
-            ... you probably shouldn't be.
-*/
+// for pages that are not in cpr's sitemap
+const cprRedirects = [
+    { source: "/about", destination: "/", permanent: true },
+    { source: "/acknowledgements", destination: "/", permanent: true },
+    { source: "/contact", destination: "/", permanent: true },
+    { source: "/methodology", destination: "/", permanent: true },
+];
+
+const defaultRedirects = [
+  {
+      source: "/auth/:id*",
+      destination: "/",
+      permanent: true,
+    },
+    {
+      source: "/account",
+      destination: "/",
+      permanent: true,
+    },
+    {
+      source: "/users/:id*",
+      destination: "/",
+      permanent: true,
+    },
+    {
+      source: "/litigation/:id*",
+      destination: "/",
+      permanent: false, // will become a page eventually
+    },
+  ];
+ 
+
+function get_redirects() {
+    const REDIRECT_FILE = process.env.NEXT_REDIRECT_FILE || "default.json";
+    const redirectsFromFile = require(`./src/redirects/${REDIRECT_FILE}`);
+    let standardRedirects = process.env.THEME === "cclw" ? cclwRedirects : cprRedirects;
+    return defaultRedirects
+      .concat(standardRedirects)
+      .concat(redirectsFromFile);
+}
 
 /**
  * @type {import('next').NextConfig}
@@ -15,29 +55,16 @@ const nextConfig = {
   },
   pageExtensions: ["tsx", "ts"],
   async redirects() {
-    return [
-      {
-        source: "/auth/:id*",
-        destination: "/",
-        permanent: true,
-      },
-      {
-        source: "/account",
-        destination: "/",
-        permanent: true,
-      },
-      {
-        source: "/users/:id*",
-        destination: "/",
-        permanent: true,
-      },
-      {
-        source: "/litigation/:id*",
-        destination: "/",
-        permanent: false, // will become a page eventually
-      },
-    ]
+    return  defaultRedirects
   },
 };
 
-module.exports = nextConfig;
+module.exports = (phase) => {
+  console.log("Starting at phase:", phase);
+  return {
+      ...nextConfig,
+      async redirects() {
+        return get_redirects();
+      }
+  }
+};
