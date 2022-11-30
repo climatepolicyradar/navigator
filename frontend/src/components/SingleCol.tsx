9a404b91ba86ed54e6cbf65572431f62f149dbe4
diversemix <diversemix@gmail.com>
2022-11-30 15:08:05 +0000

    add semicolon

--- frontend/src/components/SingleCol.tsx
+++ frontend/src/components/SingleCol.tsx
@@ -2,7 +2,7 @@ import { FC, ReactNode } from "react";
 
 type TCol = {
   children?: ReactNode;
-}
+};
 
 export const SingleCol: FC<TCol> = ({ children }) => {
   return <div className="mt-8 mx-auto max-w-screen-lg px-4">{children}</div>;
