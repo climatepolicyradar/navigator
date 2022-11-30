f8a495ea737b8aca8c906214878e625e626cabce
diversemix <diversemix@gmail.com>
2022-11-30 19:49:01 +0000

    fix - use target

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -4,10 +4,13 @@ import type { NextRequest } from 'next/server';
 import redirect_map from './redirects/redirects';
 
 export function middleware(request: NextRequest) {
-    const url = new URL(request.url); 
-    const redirect_detail = redirect_map.get(url.pathname);
-    if (redirect_detail) {
-        let status = redirect_detail['permanent'] ? 308 : 307;
-        return NextResponse.redirect(new URL(redirect_detail['target'], request.url), status)
-    }
+  const url = new URL(request.url);
+  const redirect_detail = redirect_map.get(url.pathname);
+  if (redirect_detail) {
+    let status = redirect_detail['permanent'] ? 308 : 307;
+    return NextResponse.redirect(
+      new URL(redirect_detail.destination, request.url),
+      status
+    );
+  }
 }
