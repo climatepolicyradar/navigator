2297a79235dd3f4b9a49c53e59d0413b7a35b780
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-12-08 10:40:59 +0000

    Add end of file line break

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,30 +1,30 @@
 const defaultRedirects = [
   {
-      source: "/auth/:id*",
-      destination: "/",
-      permanent: true,
-    },
-    {
-      source: "/account",
-      destination: "/",
-      permanent: true,
-    },
-    {
-      source: "/users/:id*",
-      destination: "/",
-      permanent: true,
-    },
-    {
-      source: "/litigation/:id*",
-      destination: "/",
-      permanent: false, // will become a page eventually
-    },
-  ];
- 
+    source: "/auth/:id*",
+    destination: "/",
+    permanent: true,
+  },
+  {
+    source: "/account",
+    destination: "/",
+    permanent: true,
+  },
+  {
+    source: "/users/:id*",
+    destination: "/",
+    permanent: true,
+  },
+  {
+    source: "/litigation/:id*",
+    destination: "/",
+    permanent: false, // will become a page eventually
+  },
+];
 
 /**
  * @type {import('next').NextConfig}
  */
+
 const nextConfig = {
   i18n: {
     locales: ["en", "fr"],
@@ -33,7 +33,7 @@ const nextConfig = {
   pageExtensions: ["tsx", "ts"],
   redirects: async () => {
     return defaultRedirects;
-  }
+  },
 };
 
 module.exports = nextConfig;
