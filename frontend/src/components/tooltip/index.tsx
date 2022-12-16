fd6e335ecbb27d6795a8b584b19fc3440cf0e773
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2022-12-16 09:38:00 +0000

    Feature/fro 146 display number of results (#914)
    * Add no of results indicator and tooltip
    * Use different link checker due to flakey CI test runner

--- frontend/src/components/tooltip/index.tsx
+++ frontend/src/components/tooltip/index.tsx
@@ -4,15 +4,16 @@ interface TooltipProps {
   id: string;
   tooltip: string | React.ReactNode;
   icon?: "?" | "!" | "i";
+  place?: "top" | "right" | "bottom" | "left";
 }
 
-const Tooltip = ({ id, tooltip, icon = "?" }: TooltipProps) => {
+const Tooltip = ({ id, tooltip, icon = "?", place }: TooltipProps) => {
   return (
     <div>
       <button data-tip="React-tooltip" data-for={id} className="circle-sm rounded-full bg-blue-600 text-white flex justify-center items-center text-sm font-light">
         {icon}
       </button>
-      <ReactTooltip className="customTooltip" id={id}>
+      <ReactTooltip className="customTooltip" id={id} type="light" place={place}>
         {tooltip}
       </ReactTooltip>
     </div>
