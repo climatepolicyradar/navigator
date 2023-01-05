35a5199cf8f20836bdd37ddb7d1603af0e14bd55
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-01-05 10:53:31 +0000

    Feature/remove adobe logo (#924)
    * Remove adobe partner

--- frontend/src/components/blocks/Partners.tsx
+++ frontend/src/components/blocks/Partners.tsx
@@ -7,9 +7,9 @@ const Partners = () => {
     <div className="py-24">
       <div className="max-w-screen-lg mx-auto">
         <h2 className="text-indigo-500 mb-8 text-center">Our Partners</h2>
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-8 md:gap-16 px-4 xl:px-0 mb-8 md:mb-0">
+        <div className="grid grid-cols-1 gap-8 px-4 mb-8 md:flex md:flex-wrap md:gap-[3%] md:justify-center md:mb-0 xl:px-0">
           {partners.map((partner) => (
-            <ExternalLink key={partner.link} className="block relative py-12 unset-img" url={partner.link}>
+            <ExternalLink key={partner.link} className="block relative py-12 unset-img md:basis-[22%] md:mb-8" url={partner.link}>
               <Image src={`/images/partners/${partner.logo}`} alt={partner.name} layout="fill" className="custom-img" />
             </ExternalLink>
           ))}
